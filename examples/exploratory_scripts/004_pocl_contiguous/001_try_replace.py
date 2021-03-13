68f08b78e414e052742b9a576883e443d6495c52
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-13 18:25:35 +0100

    Seems to work

--- examples/exploratory_scripts/004_pocl_contiguous/001_try_replace.py
+++ examples/exploratory_scripts/004_pocl_contiguous/001_try_replace.py
@@ -19,6 +19,8 @@ b = b_cont[1:, 1:]
 
 prg = cl.Program(ctx, """
     __kernel void copy_array_fcont(
+                 const int    ndim,
+                 const int    nelem,
         __global const int*   shape,
                  const int    itemsize,
         __global const char*  buffer_src,
@@ -30,10 +32,29 @@ prg = cl.Program(ctx, """
                       )
     {
       int gid = get_global_id(0);
-      int ibyte;
+      int ibyte, idim, this_shape, slice_size, this_index, flat_index;
+      int pos_src, pos_dest, this_stride_src, this_stride_dest;
+
+      slice_size = nelem;
+      flat_index = gid;
+      pos_src = offset_src;
+      pos_dest = offset_dest;
+      for (idim=0; idim<ndim; idim++){
+        this_shape = shape[ndim-idim-1];             // for f contiguous
+        this_stride_src = strides_src[ndim-idim-1];   // for f contiguous
+        this_stride_dest = strides_dest[ndim-idim-1]; // for f contiguous
+
+        slice_size = slice_size/this_shape;
+        this_index = flat_index/slice_size;
+        flat_index = flat_index - this_index*slice_size;
+
+        pos_src = pos_src + this_index * this_stride_src;
+        pos_dest = pos_dest + this_index * this_stride_dest;
+
+      }
 
       for (ibyte=0; ibyte<itemsize; ibyte++){
-        buffer_dest[offset_dest + ibyte] = buffer_src[offset_src + ibyte];
+        buffer_dest[pos_dest + ibyte] = buffer_src[pos_src + ibyte];
         }
     }
     """).build()
@@ -42,6 +63,8 @@ knl_copy_array_fcont = prg.copy_array_fcont
 
 assert a.shape == b.shape
 shape = cla.to_device(queue, np.array(a.shape, dtype=np.int32))
+ndim = np.int32(len(shape))
+nelem = np.int32(np.prod(a.shape))
 itemzisize = np.int32(8)
 buffer_src = a.base_data
 strides_src = cla.to_device(queue, np.array(a.strides, dtype=np.int32))
@@ -50,12 +73,9 @@ buffer_dest = b.base_data
 strides_dest = cla.to_device(queue, np.array(b.strides, dtype=np.int32))
 offset_dest = np.int32(b.offset)
 
-event = knl_copy_array_fcont(queue, (1,), None,
+event = knl_copy_array_fcont(queue, (nelem,), None,
         # args:
-        shape.data, itemzisize, buffer_src,strides_src.data, offset_src,
+        ndim,  nelem, shape.data, itemzisize,
+        buffer_src,strides_src.data, offset_src,
         buffer_dest, strides_dest.data, offset_dest)
-prrrr
-
-print('\n\n')
-print(f'{a_cont.offset=}')
-print(f'{a.offset=}')
+event.wait()
