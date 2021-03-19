89a1d317cf033936bad1f4b2e26e4b561a640c7c
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-19 17:12:06 +0100

    On documentation

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../'))
 os.system('git clone https://github.com/xsuite/xobjects')
-os.system('cd xobjects; git checkout integration_xobjects_xfields; cd ..')
+os.system('cd xobjects; git branch; git checkout integration_xobjects_xfields; cd ..')
 sys.path.insert(0, os.path.abspath('./xobjects'))
 import xobjects
 import xfields
