320a1fb0f85abb8178b42da7d71b4e101cc94be2
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-01 21:59:14 +0200

    Bassetti-Erskine, untested

---
+++ xfields/src/constants.h
@@ -0,0 +1,27 @@
+#if !defined( C_LIGHT )
+    #define   C_LIGHT ( 299792458.0 )
+#endif 
+
+#if !defined( EPSILON_0 )
+    #define   EPSILON_0 (8.854187817620e-12)
+#endif 
+
+#if !defined( PI )
+    #define PI (3.1415926535897932384626433832795028841971693993751)
+#endif 
+
+#if !defined( DEG2RAD )
+    #define DEG2RAD (0.0174532925199432957692369076848861271344287188854)
+#endif 
+
+#if !defined( RAD2DEG )
+    #define RAD2DEG (57.29577951308232087679815481410517033240547246656442)
+#endif 
+
+#if !defined( SQRT_PI )
+    #define SQRT_PI (1.7724538509055160272981674833411451827975494561224)
+#endif 
+
+#if !defined( QELEM )
+    #define QELEM (1.60217662e-19)
+#endif 
---
+++ xfields/src/fields_bigaussian.h
@@ -0,0 +1,163 @@
+#ifndef FIELDS_BIGAUSSIAN 
+#define FIELDS_BIGAUSSIAN 
+
+// for quick test with gcc
+#include "constants.h" //only_for_context none
+#include "complex_error_function.h" //only_for_context none
+
+void get_transv_field_gauss_round(
+    double sigma, double Delta_x, double Delta_y,
+    double x, double y,
+    double* Ex,
+    double* Ey)
+{
+  double r2, temp;
+
+  r2 = (x-Delta_x)*(x-Delta_x)+(y-Delta_y)*(y-Delta_y);
+  if (r2<1e-20) temp = sqrt(r2)/(2.*PI*EPSILON_0*sigma); //linearised
+  else          temp = (1-exp(-0.5*r2/(sigma*sigma)))/(2.*PI*EPSILON_0*r2);
+
+  (*Ex) = temp * (x-Delta_x);
+  (*Ey) = temp * (y-Delta_y);
+}
+
+void get_transv_field_gauss_ellip(
+        double sigma_x,  double sigma_y,
+        double Delta_x,  double Delta_y,
+        double x, double y,
+        double* Ex_out,
+        double* Ey_out)
+{
+  double sigmax = sigma_x;
+  double sigmay = sigma_y;
+
+  // I always go to the first quadrant and then apply the signs a posteriori
+  // numerically more stable (see http://inspirehep.net/record/316705/files/slac-pub-5582.pdf)
+
+  double abx = fabs(x - Delta_x);
+  double aby = fabs(y - Delta_y);
+
+  //printf("x = %.2e y = %.2e abx = %.2e aby = %.2e", xx, yy, abx, aby);
+
+  double S, factBE, Ex, Ey;
+  double etaBE_re, etaBE_im, zetaBE_re, zetaBE_im;
+  double w_etaBE_re, w_etaBE_im, w_zetaBE_re, w_zetaBE_im;
+  double expBE;
+
+  if (sigmax>sigmay){
+    S = sqrt(2.*(sigmax*sigmax-sigmay*sigmay));
+    factBE = 1./(2.*EPSILON_0*SQRT_PI*S);
+
+    etaBE_re = sigmay/sigmax*abx;
+    etaBE_im = sigmax/sigmay*aby;
+
+    zetaBE_re = abx;
+    zetaBE_im = aby;
+
+    //w_zetaBE_re, w_zetaBE_im = wfun(zetaBE_re/S, zetaBE_im/S)
+    cerrf(zetaBE_re/S, zetaBE_im/S , &(w_zetaBE_re), &(w_zetaBE_im));
+    //w_etaBE_re, w_etaBE_im = wfun(etaBE_re/S, etaBE_im/S)
+    cerrf(etaBE_re/S, etaBE_im/S , &(w_etaBE_re), &(w_etaBE_im));
+
+    expBE = exp(-abx*abx/(2*sigmax*sigmax)-aby*aby/(2*sigmay*sigmay));
+
+    Ex = factBE*(w_zetaBE_im - w_etaBE_im*expBE);
+    Ey = factBE*(w_zetaBE_re - w_etaBE_re*expBE);
+  }
+  else if (sigmax<sigmay){
+    S = sqrt(2.*(sigmay*sigmay-sigmax*sigmax));
+    factBE = 1./(2.*EPSILON_0*SQRT_PI*S);
+
+    etaBE_re = sigmax/sigmay*aby;
+    etaBE_im = sigmay/sigmax*abx;
+
+    zetaBE_re = aby;
+    zetaBE_im = abx;
+
+    //w_zetaBE_re, w_zetaBE_im = wfun(zetaBE_re/S, zetaBE_im/S)
+    cerrf(zetaBE_re/S, zetaBE_im/S , &(w_zetaBE_re), &(w_zetaBE_im));
+    //w_etaBE_re, w_etaBE_im = wfun(etaBE_re/S, etaBE_im/S)
+    cerrf(etaBE_re/S, etaBE_im/S , &(w_etaBE_re), &(w_etaBE_im));
+
+    expBE = exp(-aby*aby/(2*sigmay*sigmay)-abx*abx/(2*sigmax*sigmax));
+
+    Ey = factBE*(w_zetaBE_im - w_etaBE_im*expBE);
+    Ex = factBE*(w_zetaBE_re - w_etaBE_re*expBE);
+  }
+  else{
+    //printf("Round beam not implemented!\n");
+    //exit(1);
+    Ex = Ey = 1./0.;
+  }
+
+  if((x - Delta_x)<0) Ex=-Ex;
+  if((y - Delta_y)<0) Ey=-Ey;
+
+  (*Ex_out) = Ex;
+  (*Ey_out) = Ey;
+}
+
+
+
+void get_Ex_Ey_Gx_Gy_gauss(
+    double x, double  y,
+    double sigma_x, double sigma_y,
+    double min_sigma_diff, int skip_Gs,
+    double* Ex_ptr,
+    double* Ey_ptr,
+    double* Gx_ptr,
+    double* Gy_ptr){
+
+    double Ex, Ey, Gx, Gy;
+
+    if (fabs(sigma_x-sigma_y)< min_sigma_diff){
+
+        double sigma = 0.5*(sigma_x+sigma_y);
+
+        get_transv_field_gauss_round(sigma, 0., 0., x, y, &Ex, &Ey);
+
+        if(skip_Gs){
+          Gx = 0.;
+          Gy = 0.;
+        }
+        else{
+          Gx = 1/(2.*(x*x+y*y))*(y*Ey-x*Ex+1./(2*PI*EPSILON_0*sigma*sigma)
+                            *x*x*exp(-(x*x+y*y)/(2.*sigma*sigma)));
+          Gy = 1./(2*(x*x+y*y))*(x*Ex-y*Ey+1./(2*PI*EPSILON_0*sigma*sigma)
+                            *y*y*exp(-(x*x+y*y)/(2.*sigma*sigma)));
+        }
+
+    }
+    else{
+
+        get_transv_field_gauss_ellip(
+                sigma_x, sigma_y, 0., 0., x, y, &Ex, &Ey);
+
+        double Sig_11 = sigma_x*sigma_x;
+        double Sig_33 = sigma_y*sigma_y;
+
+        if(skip_Gs){
+          Gx = 0.;
+          Gy = 0.;
+        }
+        else{
+          Gx =-1./(2*(Sig_11-Sig_33))*(x*Ex+y*Ey+1./(2*PI*EPSILON_0)*\
+                      (sigma_y/sigma_x*exp(-x*x/(2*Sig_11)-y*y/(2*Sig_33))-1.));
+          Gy =1./(2*(Sig_11-Sig_33))*(x*Ex+y*Ey+1./(2*PI*EPSILON_0)*\
+                      (sigma_x/sigma_y*exp(-x*x/(2*Sig_11)-y*y/(2*Sig_33))-1.));
+        }
+
+    }
+
+    *Ex_ptr = Ex;
+    *Ey_ptr = Ey;
+
+    if( !skip_Gs )
+    {
+        *Gx_ptr = Gx;
+        *Gy_ptr = Gy;
+    }
+}
+
+
+#endif
