c36dd1cb25009fe4562eeb68e6c509030b9872a3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-02 22:09:39 +0200

    Added bassetti-erskine kernel to default

--- xfields/contexts/default_kernels.py
+++ xfields/contexts/default_kernels.py
@@ -48,12 +48,29 @@ kernel_descriptions = {
             (('array',  np.float64), 'particles_quantity'),),
         'num_threads_from_arg': 'nparticles'
         },
+    'get_Ex_Ey_Gx_Gy_gauss':{
+        'args':(
+            (('scalar', np.int32  ), 'n_points'),
+            (('array',  np.float64), 'x_ptr'),
+            (('array',  np.float64), 'y_ptr'),
+            (('scalar', np.float64), 'sigma_x'),
+            (('scalar', np.float64), 'sigma_y'),
+            (('scalar', np.float64), 'min_sigma_diff'),
+            (('scalar', np.int32  ), 'skip_Gs'),
+            (('array',  np.float64), 'Ex_ptr'),
+            (('array',  np.float64), 'Ey_ptr'),
+            (('array',  np.float64), 'Gx_ptr'),
+            (('array',  np.float64), 'Gy_ptr'),),
+        'num_threads_from_arg': 'n_points'
+        },
     }
 
 default_kernels = {
     'kernel_descriptions': kernel_descriptions,
     'src_files': [
-        pkg_root.joinpath('src/linear_interpolators.h')
-        ]
+        pkg_root.joinpath('src/linear_interpolators.h'),
+        pkg_root.joinpath('src/complex_error_function.h'),
+        pkg_root.joinpath('src/constants.h'),
+        pkg_root.joinpath('src/fields_bigaussian.h'),]
     }
 
