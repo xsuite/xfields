9b8c2f25ccbdf7f50ca2a852dcf5d5e094ed1513
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-02 23:22:21 +0200

    Tested all cases

--- examples/exploratory_scripts/008_test_bass_ersk_kernel/001_with_def_kernels.py
+++ examples/exploratory_scripts/008_test_bass_ersk_kernel/001_with_def_kernels.py
@@ -20,36 +20,38 @@ y = 2*np.linspace(-1, 1, 1000)
 sigma_x=0.2
 sigma_y=0.3
 
-x_dev = ctx.nparray_to_context_array(x)
-y_dev = ctx.nparray_to_context_array(y)
-Ex_dev = 0 * x_dev
-Ey_dev = 0 * y_dev
-Gx_dev = 0 * x_dev
-Gy_dev = 0 * y_dev
+for sigma_x, sigma_y in ((0.2, 0.3), (0.3, 0.2), (0.2, 0.2)):
 
-ctx.kernels.get_Ex_Ey_Gx_Gy_gauss(
-    n_points=len(x_dev),
-    x_ptr=x_dev,
-    y_ptr=y_dev,
-    sigma_x=sigma_x,
-    sigma_y=sigma_y,
-    min_sigma_diff=1e-5,
-    skip_Gs=0,
-    Ex_ptr=Ex_dev,
-    Ey_ptr=Ey_dev,
-    Gx_ptr=Gx_dev,
-    Gy_ptr=Gy_dev,
-    )
+    x_dev = ctx.nparray_to_context_array(x)
+    y_dev = ctx.nparray_to_context_array(y)
+    Ex_dev = 0 * x_dev
+    Ey_dev = 0 * y_dev
+    Gx_dev = 0 * x_dev
+    Gy_dev = 0 * y_dev
 
-Ex = ctx.nparray_from_context_array(Ex_dev)
-Ey = ctx.nparray_from_context_array(Ey_dev)
-Gx = ctx.nparray_from_context_array(Gx_dev)
-Gy = ctx.nparray_from_context_array(Gy_dev)
+    ctx.kernels.get_Ex_Ey_Gx_Gy_gauss(
+        n_points=len(x_dev),
+        x_ptr=x_dev,
+        y_ptr=y_dev,
+        sigma_x=sigma_x,
+        sigma_y=sigma_y,
+        min_sigma_diff=1e-5,
+        skip_Gs=0,
+        Ex_ptr=Ex_dev,
+        Ey_ptr=Ey_dev,
+        Gx_ptr=Gx_dev,
+        Gy_ptr=Gy_dev,
+        )
 
-Ex_pst, Ey_pst, Gx_pst, Gy_pst = get_Ex_Ey_Gx_Gy_gauss(x, y, sigma_x, sigma_y,
-        min_sigma_diff=1e-5, skip_Gs=False, mathlib=MathlibDefault())
+    Ex = ctx.nparray_from_context_array(Ex_dev)
+    Ey = ctx.nparray_from_context_array(Ey_dev)
+    Gx = ctx.nparray_from_context_array(Gx_dev)
+    Gy = ctx.nparray_from_context_array(Gy_dev)
 
-assert np.allclose(Ex, Ex_pst)
-assert np.allclose(Ey, Ey_pst)
-assert np.allclose(Gx, Gx_pst)
-assert np.allclose(Gy, Gy_pst)
+    Ex_pst, Ey_pst, Gx_pst, Gy_pst = get_Ex_Ey_Gx_Gy_gauss(x, y, sigma_x, sigma_y,
+            min_sigma_diff=1e-5, skip_Gs=False, mathlib=MathlibDefault())
+
+    assert np.allclose(Ex, Ex_pst)
+    assert np.allclose(Ey, Ey_pst)
+    assert np.allclose(Gx, Gx_pst)
+    assert np.allclose(Gy, Gy_pst)
