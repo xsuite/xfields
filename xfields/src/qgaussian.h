375b34422e7e8b2313447acaad7a469fd16f50ce
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-11 12:18:23 +0200

    Working on qgaussian profile

---
+++ xfields/src/qgaussian.h
@@ -0,0 +1,12 @@
+
+void q_gaussian_profile(
+		    const double* z,
+		    const double sqrt_beta, 
+		    const double q, 
+		    const double cq,
+		    const double N,
+		          double* res){
+
+    double const exp_q_arg =  -( sqrt_beta * sqrt_beta * z * z );
+
+}	
