e33afff15b5c29c1cb28901d3fbfd83bd57904ab
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-11 22:47:21 +0200

    Qgaussian compiles, untested

--- xfields/src/qgaussian.h
+++ xfields/src/qgaussian.h
@@ -5,21 +5,39 @@
 void q_gaussian_profile(
 		    const int n,
 		    const double* z,
+		    const double z0,
+		    const double z_min,
+		    const double z_max,
 		    const double beta, 
 		    const double q, 
 		    const double q_tol,
 		    const double factor,
 		          double* res){
     if (fabs(q-1.) < q_tol){
-
+    	for(int ii; ii<n; ii++){ //vectorize_over ii n 
+    	    double zi = z[ii];
+	    if (zi<z_max && zi>z_min){
+		double zi_m_z0 = zi - z0;
+	    	res[ii] = factor*exp(-beta*zi_m_z0*zi_m_z0 );
+	    }
+	    else{
+	    	res[ii] = 0; 
+	    }
+    	}//end_vectorize
     }
-    else {
+    else{
     	double exponent = 1./(1.-q);
-    	for(int ii; ii<n; ii++){ //vectorize_over ii 
-    	    double zi = z[ii]
-    	    double q_exp_arg =  -( beta * zi * zi );
-    	    double q_exp_res = pow(q_exp_arg, exponent );
-    	    res[ii] = factor*q_exp_res;
+    	for(int ii; ii<n; ii++){ //vectorize_over ii n
+    	    double zi = z[ii];
+	    if (zi<z_max && zi>z_min){
+		double zi_m_z0 = zi - z0;
+    	    	double q_exp_arg =  -(beta*zi_m_z0*zi_m_z0 );
+    	    	double q_exp_res = pow(q_exp_arg, exponent );
+    	    	res[ii] = factor*q_exp_res;
+	    }
+	    else{
+	    	res[ii] = 0; 
+	    }
     	}//end_vectorize
     }
 }	
