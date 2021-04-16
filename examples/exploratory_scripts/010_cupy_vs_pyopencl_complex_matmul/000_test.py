1baf9deee13de4e089d81b4550e07adfd3e06cbc
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-16 22:08:13 +0200

    Spotted

--- examples/exploratory_scripts/010_cupy_vs_pyopencl_complex_matmul/000_test.py
+++ examples/exploratory_scripts/010_cupy_vs_pyopencl_complex_matmul/000_test.py
@@ -7,39 +7,26 @@ size_y = 512
 size_z = 100
 
 context = xo.ContextCupy()
+#context = xo.ContextPyopencl()
+#context = xo.ContextCpu()
+print(context)
 
-a = context.zeros((size_x, size_y, size_z), dtype=np.complex128, order='F')
-b = context.zeros((size_x, size_y, size_z), dtype=np.complex128, order='F')
+for order in ('F', 'C'):
+    a = context.zeros((size_x, size_y, size_z),
+            dtype=np.complex128, order=order)
+    b = context.zeros((size_x, size_y, size_z),
+            dtype=np.complex128, order=order)
+    print(f'\n\n{order=}')
+    for _ in range(5):
+        t1 = time.time()
+        a[:,:,:] *= b
+        context.synchronize()
+        t2 = time.time()
+        print(f'Time "a[:,:,:] *= b":     {(t2-t1)*1000:.2f} ms')
 
+        t1 = time.time()
+        a.T[:,:,:] *= b.T
+        context.synchronize()
+        t2 = time.time()
+        print(f'Time "a.T[:,:,:] *= b.T": {(t2-t1)*1000:.2f} ms')
 
-#sigma_x = 3e-3
-#sigma_y = 2e-3
-#sigma_z = 30e-2
-#
-#x_lim = 5.*sigma_x
-#y_lim = 5.*sigma_y
-#z_lim = 5.*sigma_z
-#
-#from xfields import SpaceCharge3D
-#
-#spcharge = SpaceCharge3D(
-#        length=1, update_on_track=True, apply_z_kick=False,
-#        x_range=(-x_lim, x_lim),
-#        y_range=(-y_lim, y_lim),
-#        z_range=(-z_lim, z_lim),
-#        nx=256, ny=256, nz=100,
-#        solver='FFTSolver2p5D',
-#        context=context)
-#
-#a[:,:,:] = spcharge.fieldmap.solver._workspace_dev
-#b[:,:,:] = spcharge.fieldmap.solver._gint_rep_transf_dev
-#
-#a = spcharge.fieldmap.solver._workspace_dev.T
-#b = spcharge.fieldmap.solver._gint_rep_transf_dev.T
-
-for _ in range(5):
-    t1 = time.time()
-    a[:,:,:] = a*b
-    context.synchronize()
-    t2 = time.time()
-    print(f'{(t2-t1)*1000} ms')
