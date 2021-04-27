be6b981cc9cb5da73c834985c07ba2383dcdce7c
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-27 23:21:24 +0200

    Adapting to new xobjects kernel api

--- xfields/contexts/add_default_kernels.py
+++ xfields/contexts/add_default_kernels.py
@@ -8,8 +8,8 @@ def add_default_kernels(context):
     for kk in default_kernels['kernel_descriptions'].keys():
         if kk not in context.kernels.keys():
             context.add_kernels(
-                src_files=default_kernels['src_files'],
-                kernel_descriptions=default_kernels['kernel_descriptions'])
+                sources=default_kernels['src_files'],
+                kernels=default_kernels['kernel_descriptions'])
             break
 
     for kk in default_kernels['kernel_descriptions'].keys():
