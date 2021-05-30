311ac4d32b8e1b48ff6de592973adf7251f5760d
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-30 22:30:48 +0200

    Ported two more functions, still untested

--- xfields/beam_elements/beambeam_src/beambeam3d.h
+++ xfields/beam_elements/beambeam_src/beambeam3d.h
@@ -156,4 +156,140 @@ void Sigmas_propagate(
 
 }
 
+/*gpufun*/
+void BoostParameters_boost_coordinates(
+    const BoostParameters bp,
+    double* x_star,
+    double* px_star,
+    double* y_star,
+    double* py_star,
+    double* sigma_star,
+    double* delta_star){
+
+    double const sphi = BoostParameters_get_sphi(bp);
+    double const cphi = BoostParameters_get_cphi(bp);
+    double const tphi = BoostParameters_get_tphi(bp);
+    double const salpha = BoostParameters_get_salpha(bp);
+    double const calpha = BoostParameters_get_calpha(bp);
+
+    double const x = *x_star;
+    double const px = *px_star;
+    double const y = *y_star;
+    double const py = *py_star ;
+    double const sigma = *sigma_star;
+    double const delta = *delta_star ;
+
+    double const h = delta + 1. - sqrt((1.+delta)*(1.+delta)-px*px-py*py);
+
+
+    double const px_st = px/cphi-h*calpha*tphi/cphi;
+    double const py_st = py/cphi-h*salpha*tphi/cphi;
+    double const delta_st = delta -px*calpha*tphi-py*salpha*tphi+h*tphi*tphi;
+
+    double const pz_st =
+        sqrt((1.+delta_st)*(1.+delta_st)-px_st*px_st-py_st*py_st);
+
+    double const hx_st = px_st/pz_st;
+    double const hy_st = py_st/pz_st;
+    double const hsigma_st = 1.-(delta_st+1)/pz_st;
+
+    double const L11 = 1.+hx_st*calpha*sphi;
+    double const L12 = hx_st*salpha*sphi;
+    double const L13 = calpha*tphi;
+
+    double const L21 = hy_st*calpha*sphi;
+    double const L22 = 1.+hy_st*salpha*sphi;
+    double const L23 = salpha*tphi;
+
+    double const L31 = hsigma_st*calpha*sphi;
+    double const L32 = hsigma_st*salpha*sphi;
+    double const L33 = 1./cphi;
+
+    double const x_st = L11*x + L12*y + L13*sigma;
+    double const y_st = L21*x + L22*y + L23*sigma;
+    double const sigma_st = L31*x + L32*y + L33*sigma;
+
+    *x_star = x_st;
+    *px_star = px_st;
+    *y_star = y_st;
+    *py_star = py_st;
+    *sigma_star = sigma_st;
+    *delta_star = delta_st;
+
+}
+
+/*gpufun*/
+void BoostParameters_boost_coordinates(
+    	const BoostParameters bp,
+    	double* x,
+    	double* px,
+    	double* y,
+    	double* py,
+    	double* sigma,
+    	double* delta){
+
+    double const sphi = BoostParameters_get_sphi(bp);
+    double const cphi = BoostParameters_get_cphi(bp);
+    double const tphi = BoostParameters_get_tphi(bp);
+    double const salpha = BoostParameters_get_salpha(bp);
+    double const calpha = BoostParameters_get_calpha(bp);
+
+    double const x_st = *x;
+    double const px_st = *px;
+    double const y_st = *y;
+    double const py_st = *py ;
+    double const sigma_st = *sigma;
+    double const delta_st = *delta ;
+
+    double const pz_st = sqrt((1.+delta_st)*(1.+delta_st)-px_st*px_st-py_st*py_st);
+    double const hx_st = px_st/pz_st;
+    double const hy_st = py_st/pz_st;
+    double const hsigma_st = 1.-(delta_st+1)/pz_st;
+
+    double const Det_L =
+        1./cphi + (hx_st*calpha + hy_st*salpha-hsigma_st*sphi)*tphi;
+
+    double const Linv_11 =
+        (1./cphi + salpha*tphi*(hy_st-hsigma_st*salpha*sphi))/Det_L;
+
+    double const Linv_12 =
+        (salpha*tphi*(hsigma_st*calpha*sphi-hx_st))/Det_L;
+
+    double const Linv_13 =
+        -tphi*(calpha - hx_st*salpha*salpha*sphi + hy_st*calpha*salpha*sphi)/Det_L;
+
+    double const Linv_21 =
+        (calpha*tphi*(-hy_st + hsigma_st*salpha*sphi))/Det_L;
+
+    double const Linv_22 =
+        (1./cphi + calpha*tphi*(hx_st-hsigma_st*calpha*sphi))/Det_L;
+
+    double const Linv_23 =
+        -tphi*(salpha - hy_st*calpha*calpha*sphi + hx_st*calpha*salpha*sphi)/Det_L;
+
+    double const Linv_31 = -hsigma_st*calpha*sphi/Det_L;
+    double const Linv_32 = -hsigma_st*salpha*sphi/Det_L;
+    double const Linv_33 = (1. + hx_st*calpha*sphi + hy_st*salpha*sphi)/Det_L;
+
+    double const x_i = Linv_11*x_st + Linv_12*y_st + Linv_13*sigma_st;
+    double const y_i = Linv_21*x_st + Linv_22*y_st + Linv_23*sigma_st;
+    double const sigma_i = Linv_31*x_st + Linv_32*y_st + Linv_33*sigma_st;
+
+    double const h = (delta_st+1.-pz_st)*cphi*cphi;
+
+    double const px_i = px_st*cphi+h*calpha*tphi;
+    double const py_i = py_st*cphi+h*salpha*tphi;
+
+    double const delta_i = delta_st + px_i*calpha*tphi + py_i*salpha*tphi - h*tphi*tphi;
+
+
+    *x = x_i;
+    *px = px_i;
+    *y = y_i;
+    *py = py_i;
+    *sigma = sigma_i;
+    *delta = delta_i;
+
+}
+
 #endif
