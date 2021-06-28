334a1ec6df4c56cd1e3de0c1f6c2b38e4481efa5
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-28 16:57:41 +0200

    Added spacecharge helper function

--- xfields/__init__.py
+++ xfields/__init__.py
@@ -11,3 +11,4 @@ from .beam_elements.beambeam import BeamBeamBiGaussian2D
 from .beam_elements.beambeam3d import BeamBeamBiGaussian3D
 
 from .general import _pkg_root
+from .config_tools import replace_spaceharge_with_quasi_frozen
