103beace7bd8e7a548cebf23d3d456df1a2ade5e
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-07-29 15:56:55 +0200

    Beam-beam scales nicely

--- examples/002_beambeam/000_beambeam.py
+++ examples/002_beambeam/000_beambeam.py
@@ -12,9 +12,9 @@ import xtrack as xt
 
 context = ContextCpu(omp_num_threads=0) # no omp
 #context = ContextCpu(omp_num_threads=1) # omp
-#context = ContextCpu(omp_num_threads=48) # omp
-context = ContextCupy(default_block_size=256)
-context = ContextPyopencl('0.0')
+context = ContextCpu(omp_num_threads=8) # omp
+#context = ContextCupy(default_block_size=256)
+#context = ContextPyopencl('0.0')
 
 print(repr(context))
 
