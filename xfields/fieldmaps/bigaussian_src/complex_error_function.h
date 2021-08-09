7c4d2da923146483f1fd2e627946a81c79196b0f
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-08-09 23:57:09 +0200

    replaces CERNLib C version of the Faddeeva function with an optimised version
    - Removes the three-way split of the main loop, replace by two loops that
      are run sequentially (the second loop is only executed if z is within
      the rectangle (0,FADDEEVA_X_LIMIT) * (0,FADDEEVA_Y_LIMIT)
    
    - Un-splitting the loop allows to get rid of the Rx/Ry arrays
      that were used to store the intermediate results from one loop ->
      these arrays caused massive resource problems on GPUs and limited
      scalability
    
    - Use a hand-rolled power function for integer arguments ->
      this is (generally) more efficient than using the general
      double-precision exp/log based variant.
    
    - Simplify the implementation of the symmetry conditions to
      extend the coverage from Q1 to other quadrants -> this
      requires less if/else clauses than the original implementation
    
    - Replace all magic constants and constants from the original
      implementation with macros -> this allows to tune the
      loop boundaries and emulate the upstream cernlib cwerf function
      (C335 from the genlib/c module)
    
    - Where feasibble, branchless implementations of algorithms
      have been used

--- xfields/fieldmaps/bigaussian_src/complex_error_function.h
+++ xfields/fieldmaps/bigaussian_src/complex_error_function.h
@@ -1,86 +1,157 @@
-#ifndef COMPLEX_ERROR_FUNCTION_H 
-#define COMPLEX_ERROR_FUNCTION_H 
+#ifndef XFIELDS_COMPLEX_ERROR_FUNCTION_H
+#define XFIELDS_COMPLEX_ERROR_FUNCTION_H
 
+#include <stdbool.h> //only_for_context cpu_serial cpu_openmp
 #include <math.h> //only_for_context cpu_serial cpu_openmp
 
+#include "constants.h"
+
 /*gpufun*/
-void cerrf(
-    double in_real, 
-    double in_imag,
-    double* out_real,
-    double* out_imag ){
-    /* This function calculates the double precision complex error fnct.
-    based on the algorithm of the FORTRAN function written at CERN by K. Koelbig
-    Program C335, 1970. See also M. Bassetti and G.A. Erskine, "Closed
-    expression for the electric field of a two-dimensional Gaussian charge
-    density", CERN-ISR-TH/80-06; */
+void cerrf( double x, double y,
+    double* /*restrict*/ out_x,
+    double* /*restrict*/ out_y )
+{
+    typedef double real_type;
 
-   int n, nc, nu;
-   double a_constant = 1.12837916709551;
-   double xLim = 5.33;
-   double yLim = 4.29;
-   double h, q, Saux, Sx, Sy, Tn, Tx, Ty, Wx, Wy, xh, xl, x, yh, y;
-   double Rx [33];
-   double Ry [33];
+    /**
+     * Based upon the algorithm developed by W. Gautschi 1970,
+     * "Efficient Computation of the Complex Error Function",
+     * SIAM Journal on Numerical Analysis, Vol. 7, Issue 1. 1970, pages 187-198
+     * https://epubs.siam.org/doi/10.1137/0707012
+     *
+     * It corresponds closely to the previously used "CERNLib C" version,
+     * translated from the FORTRAN function written at CERN by K. Koelbig,
+     * Program C335, 1970. The main difference to Gautschi's formulation is
+     * a split in the main loop and the introduction of arrays to store the
+     * intermediate results as a consequence of this. The version implemented
+     * here should perform roughly equally well or even slightly better on
+     * modern out-of-order super-scalar CPUs but has drastically improved
+     * performance on GPUs and GPU-like systems.
+     *
+     * See also M. Bassetti and G.A. Erskine,
+     * "Closed expression for the electric field of a two-dimensional Gaussian
+     *  charge density", CERN-ISR-TH/80-06; */
 
-   x = fabs(in_real);
-   y = fabs(in_imag);
+    real_type const sign_x = ( real_type )( x >= ( real_type )0.0 ) -
+                             ( real_type )( x <  ( real_type )0.0 );
 
-   if (y < yLim && x < xLim){
-       q = (1.0 - y / yLim) * sqrt(1.0 - (x / xLim) * (x / xLim));
-       h  = 1.0 / (3.2 * q);
-       nc = 7 + (int) (23.0 * q);
-       xl = pow(h, (double) (1 - nc));
-       xh = y + 0.5 / h;
-       yh = x;
-       nu = 10 + (int) (21.0 * q);
-       Rx[nu] = 0.;
-       Ry[nu] = 0.;
-       for (n = nu; n > 0; n--){
-           Tx = xh + n * Rx[n];
-           Ty = yh - n * Ry[n];
-           Tn = Tx*Tx + Ty*Ty;
-           Rx[n-1] = 0.5 * Tx / Tn;
-           Ry[n-1] = 0.5 * Ty / Tn;
-           }
-       /* .... */
-       Sx = 0.;
-       Sy = 0.;
-       for (n = nc; n>0; n--){
-           Saux = Sx + xl;
-           Sx = Rx[n-1] * Saux - Ry[n-1] * Sy;
-           Sy = Rx[n-1] * Sy + Ry[n-1] * Saux;
-           xl = h * xl;
-       };
-       Wx = a_constant * Sx;
-       Wy = a_constant * Sy;
-   }
-   else{
-       xh = y;
-       yh = x;
-       Rx[0] = 0.;
-       Ry[0] = 0.;
-       for (n = 9; n>0; n--){
-           Tx = xh + n * Rx[0];
-           Ty = yh - n * Ry[0];
-           Tn = Tx * Tx + Ty * Ty;
-           Rx[0] = 0.5 * Tx / Tn;
-           Ry[0] = 0.5 * Ty / Tn;
-       };
-       Wx = a_constant * Rx[0];
-       Wy = a_constant * Ry[0];
-   }
-   if (y == 0.) {Wx = exp(-x * x);}
-   if (in_imag < 0.){
-       Wx =   2.0 * exp(y * y - x * x) * cos(2.0 * x * y) - Wx;
-       Wy = - 2.0 * exp(y * y - x * x) * sin(2.0 * x * y) - Wy;
-       if (in_real > 0.) {Wy = -Wy;}
-   }
-   else if (in_real < 0.) {Wy = -Wy;}
+    real_type const sign_y = ( real_type )( y >= ( real_type )0.0 ) -
+                             ( real_type )( y <  ( real_type )0.0 );
 
-   *out_real = Wx;
-   *out_imag = Wy;
+    real_type inv_h2   = ( real_type )1.0;
+    real_type temp     = y * sign_y;
+    real_type y_plus_h = temp;
+
+    real_type Rx, Ry, Sx, Sy, Wx, Wy, h2_n, nn;
+
+    int nu = ( int )FADDEEVA_GAUSS_HERMITE_NU;
+    int N  = 0;
+    int n  = 0;
+
+    bool z_is_in_r0, use_taylor_sum;
+    Ry = Sx = Sy = h2_n = ( real_type )0.0;
+
+    y  = temp;
+	x *= sign_x;
+
+    /* R_0 ... rectangle with width FADDEEVA_X_LIMIT and
+     *         height FADDEEVA_Y_LIMIT. Inside R_0, w(z) is calculated using
+     *         a truncated Taylor expansion. Outside, a Gauss--Hermite
+     *         quadrature in the guise of a continuos fraction is used */
+
+    z_is_in_r0 = ( ( y < ( real_type )FADDEEVA_Y_LIMIT ) &&
+                   ( x < ( real_type )FADDEEVA_X_LIMIT ) );
+
+	if( z_is_in_r0 )
+    {
+        temp = x / ( real_type )FADDEEVA_X_LIMIT;
+        temp = ( ( real_type )1.0 - y / ( real_type )FADDEEVA_Y_LIMIT ) *
+               sqrt( ( real_type )1.0 - temp * temp );
+
+
+        nu   = ( y > ( real_type )REAL_EPS )
+             ? ( int )FADDEEVA_NU_0 + ( int )( ( real_type )FADDEEVA_NU_1 * temp )
+             : ( int )0;
+
+        N    = ( int )FADDEEVA_N0 + ( int )( ( real_type )FADDEEVA_N1 * temp );
+        h2_n = ( real_type )2.0 * ( real_type )FADDEEVA_H0 * temp;
+        inv_h2 = ( real_type )1.0 / h2_n;
+        y_plus_h += ( real_type )0.5 * h2_n;
+        h2_n = power_n( h2_n, N - 1 );
+    }
+
+    Rx = ( y > ( real_type )REAL_EPS )
+       ? ( real_type )0.0
+       : exp( -x * x ) / ( real_type )TWO_OVER_SQRT_PI;
+
+    use_taylor_sum = ( z_is_in_r0 ) && ( h2_n > ( real_type )REAL_EPS );
+
+    n = nu;
+    nn = ( real_type )n;
+
+    /* z outside of R_0: continuos fraction / Gauss - Hermite quadrature
+     * z inside  of R_0: first iterations of recursion until n == N */
+    for( ; n > N ; --n, nn -= ( real_type )1.0 )
+    {
+        Wx     = y_plus_h + nn * Rx;
+        Wy     = x - nn * Ry;
+        temp   = ( Wx * Wx + Wy * Wy );
+        Rx     = ( real_type )0.5 * Wx;
+        Ry     = ( real_type )0.5 * Wy;
+        temp   = ( real_type )1.0 / temp;
+        Rx    *= temp;
+        Ry    *= temp;
+    }
+
+    /* loop rejects everything if z is not in R_0 because then n == 0 already;
+     * otherwise, N iterations until taylor expansion is summed up */
+    for( ; n > 0 ; --n, nn -= ( real_type )1.0 )
+    {
+        Wx     = y_plus_h + nn * Rx;
+        Wy     = x - nn * Ry;
+        temp   = ( Wx * Wx + Wy * Wy );
+        Rx     = ( real_type )0.5 * Wx;
+        Ry     = ( real_type )0.5 * Wy;
+        temp   = ( real_type )1.0 / temp;
+        Rx    *= temp;
+        Ry    *= temp;
+
+        Wx     = h2_n + Sx;
+        h2_n  *= inv_h2;
+        Sx     = Rx * Wx - Ry * Sy;
+        Sy     = Ry * Wx + Rx * Sy;
+    }
+
+    /* Wx, Wy ... result for z|Q1 = |x| + i |y| ... in first quadrant! */
+    Wx = ( real_type )TWO_OVER_SQRT_PI * ( ( use_taylor_sum ) ? Sx : Rx );
+    Wy = ( real_type )TWO_OVER_SQRT_PI * ( ( use_taylor_sum ) ? Sy : Ry );
+
+    if( sign_y < ( real_type )0.0 )  /* Quadrants 3 & 4 */
+    {
+        real_type const arg = ( real_type )2.0 * x * y;
+
+        #if defined( __OPENCL_VERSION__ ) || \
+                   ( ( defined( XFIELDS_USE_SINCOS ) ) && \
+                     ( XFIELDS_USE_SINCOS == 1 ) )
+        real_type exp_cos;
+        real_type exp_sin = sincos( arg, &exp_cos );
+        #else
+        real_type exp_cos = cos( arg );
+        real_type exp_sin = sin( arg );
+        #endif
+
+        temp = ( y - x ) * ( x + y );
+        temp = exp( temp );
+        exp_cos *= temp;
+        exp_sin *= temp;
+
+        Wx  = exp_cos - Wx;
+        Wy += exp_sin;
+    }
+
+    *out_x = Wx;
+    *out_y = sign_x * Wy;
 }
 
-#endif 
+#endif /* XFIELDS_COMPLEX_ERROR_FUNCTION_H */
 
