af41d900e1d8b345beb37e63992336795166cae7
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-08-29 20:55:08 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = []
 
 setup(
     name='xfields',
-    version='0.2.0',
+    version='0.2.1',
     description='Field Maps and Particle In Cell',
     url='https://github.com/xsuite/xfields',
     author='Giovanni Iadarola',
