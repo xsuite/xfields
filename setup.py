a200758fd15046287f175b4c1f9e05971fc65755
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-18 10:48:16 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = []
 
 setup(
     name='xfields',
-    version='0.2.2',
+    version='0.3.0',
     description='Field Maps and Particle In Cell',
     url='https://github.com/xsuite/xfields',
     author='Giovanni Iadarola',
