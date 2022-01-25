ac858c3fa39f24a7d2774a5410faa22eabc2739e
Konstantinos Paraschou <konstantinos.paraschou@cern.ch>
2022-01-25 17:22:36 +0100

    added imports in __init__ files

--- xfields/__init__.py
+++ xfields/__init__.py
@@ -2,6 +2,7 @@ from .longitudinal_profiles import LongitudinalProfileCoasting
 from .longitudinal_profiles import LongitudinalProfileQGaussian
 
 from .fieldmaps import TriLinearInterpolatedFieldMap
+from .fieldmaps import TriCubicInterpolatedFieldMap
 from .fieldmaps import BiGaussianFieldMap, mean_and_std
 
 from .solvers.fftsolvers import FFTSolver3D
@@ -9,6 +10,7 @@ from .solvers.fftsolvers import FFTSolver3D
 from .beam_elements.spacecharge import SpaceCharge3D, SpaceChargeBiGaussian
 from .beam_elements.beambeam import BeamBeamBiGaussian2D
 from .beam_elements.beambeam3d import BeamBeamBiGaussian3D
+from .beam_elements.electroncloud import ElectronCloud
 
 from .general import _pkg_root
 from .config_tools import replace_spacecharge_with_quasi_frozen
