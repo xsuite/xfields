a235af9c2c499a54ecca52317bd71de54e026659
giadarol <giovanni.iadarola@cern.ch>
2022-08-12 18:08:13 +0200

    A rename

--- xfields/beam_elements/beambeam3d.py
+++ xfields/beam_elements/beambeam3d.py
@@ -84,7 +84,7 @@ class BeamBeamBiGaussian3D(xt.BeamElement):
         _pkg_root.joinpath('beam_elements/beambeam_src/beambeam3d_methods_for_strongstrong.h'),
     ]
 
-    per_particle_kernels={
+    _per_particle_kernels={
         'synchro_beam_kick': xo.Kernel(
             c_name='BeamBeam3D_selective_apply_synchrobeam_kick_local_particle',
             args=[
