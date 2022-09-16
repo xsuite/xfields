fd2501df621f20a2edc097f7853b587ae9ae640d
Szymon Łopaciuk <szymon@lopaciuk.eu>
2022-09-16 09:37:16 +0200

    test faddeeva using an xo.HybridClass calculator

--- tests/test_cerrf.py
+++ tests/test_cerrf.py
@@ -5,14 +5,90 @@
 
 import numpy as np
 from scipy.special import wofz as wofz_scipy
+import pytest
 import xobjects as xo
 from xobjects.context import available
 from xfields.general import _pkg_root
 
 
-def test_cerrf_q1():
-    ctx = xo.ContextCpu(omp_num_threads=0)
+@pytest.fixture
+def faddeeva_calculator():
+    source = '''
+        /*gpukern*/ void FaddeevaCalculator_compute(FaddeevaCalculatorData data) {
+            int64_t len = FaddeevaCalculatorData_len_z_re(data);
 
+            for (int64_t ii = 0; ii < len; ii++) {  //vectorize_over ii len
+                double z_re = FaddeevaCalculatorData_get_z_re(data, ii);
+                double z_im = FaddeevaCalculatorData_get_z_im(data, ii);
+                double w_re, w_im;
+
+                cerrf(z_re, z_im, &w_re, &w_im);
+
+                FaddeevaCalculatorData_set_w_re(data, ii, w_re);
+                FaddeevaCalculatorData_set_w_im(data, ii, w_im);
+            } //end_vectorize
+        }
+    '''
+
+    class FaddeevaCalculator(xo.HybridClass):
+        _xofields = {
+            'z_re': xo.Float64[:],
+            'z_im': xo.Float64[:],
+            'w_re': xo.Float64[:],
+            'w_im': xo.Float64[:],
+        }
+
+        _extra_c_sources = [
+            _pkg_root.joinpath("headers/constants.h"),
+            _pkg_root.joinpath("headers/sincos.h"),
+            _pkg_root.joinpath("headers/power_n.h"),
+            _pkg_root.joinpath("fieldmaps/bigaussian_src/complex_error_function.h"),
+            source,
+        ]
+
+        _kernels = {
+            'FaddeevaCalculator_compute': xo.Kernel(
+                args=[
+                    xo.Arg(xo.ThisClass, name='data'),
+                ],
+            )
+        }
+
+        def __init__(self, z, **kwargs):
+            z = np.array(z)
+
+            self.xoinitialize(
+                z_re=z.real,
+                z_im=z.imag,
+                w_re=len(z),
+                w_im=len(z),
+                **kwargs,
+            )
+
+        @property
+        def w(self):
+            re = self._context.nparray_from_context_array(self.w_re)
+            im = self._context.nparray_from_context_array(self.w_im)
+            return re + 1j * im
+
+        def compute(self):
+            self._xobject.compile_kernels(only_if_needed=True)
+            kernel = self._context.kernels.FaddeevaCalculator_compute
+            kernel.description.n_threads = len(self.z_re)
+            kernel(data=self)
+
+    return FaddeevaCalculator
+
+
+@pytest.mark.parametrize(
+    'context',
+    xo.context.get_test_contexts(),
+    ids=[str(ctx) for ctx in xo.context.get_test_contexts()],
+)
+def test_cerrf_q1(faddeeva_calculator, context):
+    FaddeevaCalculator = faddeeva_calculator
+
+    # Generate the test grid
     xx = np.concatenate(([0], np.logspace(-8, +8, 51))).astype(np.float64)
     yy = np.concatenate(([0], np.logspace(-8, +8, 51))).astype(np.float64)
 
@@ -21,6 +97,11 @@ def test_cerrf_q1():
     n_z = len(yy) * len(xx)
 
     re_absc, im_absc = np.meshgrid(xx, yy)
+
+    # Calculate the values based on the grid
+    z = (re_absc + 1j * im_absc).reshape(n_re * n_im)
+    calculator = FaddeevaCalculator(z=z, _context=context)
+    calculator.compute()
 
     # Using scipy's wofz implemenation of the Faddeeva method. This is
     # (at the time of this writing in 2021) based on the MIT ab-initio
@@ -31,68 +112,8 @@ def test_cerrf_q1():
     # in the *absolute* error.
     wz_cmp = wofz_scipy(re_absc + 1.0j * im_absc)
 
-    src_code = """
-    /*gpukern*/ void eval_cerrf_q1(
-        const int n,
-        /*gpuglmem*/ double const* /*restrict*/ re,
-        /*gpuglmem*/ double const* /*restrict*/ im,
-        /*gpuglmem*/ double* /*restrict*/ wz_re,
-        /*gpuglmem*/ double* /*restrict*/ wz_im )
-    {
-        for(int tid = 0; ; tid < n ; ++tid ) { //vectorize_over tid n
-            if( tid < n )
-            {
-                double const x = re[ tid ];
-                double const y = im[ tid ];
-                double wz_x, wz_y;
-
-                cerrf_q1( x, y, &wz_x, &wz_y );
-
-                wz_re[ tid ] = wz_x;
-                wz_im[ tid ] = wz_y;
-            }
-        } //end_vectorize
-    }
-    """
-
-    kernel_descriptions = {
-        "eval_cerrf_q1": xo.Kernel(
-            args=[
-                xo.Arg(xo.Int32, name="n"),
-                xo.Arg(xo.Float64, name="re", const=True, pointer=True),
-                xo.Arg(xo.Float64, name="im", const=True, pointer=True),
-                xo.Arg(xo.Float64, name="wz_re", pointer=True),
-                xo.Arg(xo.Float64, name="wz_im", pointer=True),
-            ],
-            n_threads="n",
-        ),
-    }
-
-    headers = [
-        _pkg_root.joinpath("headers/constants.h"),
-        _pkg_root.joinpath("headers/sincos.h"),
-        _pkg_root.joinpath("headers/power_n.h"),
-        _pkg_root.joinpath("fieldmaps/bigaussian_src/complex_error_function.h"),
-    ]
-
-    wz_re = np.arange(n_z, dtype=np.float64)
-    wz_im = np.arange(n_z, dtype=np.float64)
-
-    re_absc_dev = ctx.nparray_to_context_array(re_absc.reshape(n_z))
-    im_absc_dev = ctx.nparray_to_context_array(im_absc.reshape(n_z))
-    wz_re_dev = ctx.nparray_to_context_array(wz_re)
-    wz_im_dev = ctx.nparray_to_context_array(wz_im)
-
-    ctx.add_kernels(
-        sources=[src_code], kernels=kernel_descriptions, extra_headers=headers
-    )
-
-    ctx.kernels.eval_cerrf_q1(
-        n=n_z, re=re_absc_dev, im=im_absc_dev, wz_re=wz_re_dev, wz_im=wz_im_dev
-    )
-
-    wz_re = ctx.nparray_from_context_array(wz_re_dev).reshape(n_im, n_re)
-    wz_im = ctx.nparray_from_context_array(wz_im_dev).reshape(n_im, n_re)
+    wz_re = calculator.w.real.reshape(n_im, n_re)
+    wz_im = calculator.w.imag.reshape(n_im, n_re)
 
     d_abs_re = np.fabs(wz_re - wz_cmp.real)
     d_abs_im = np.fabs(wz_im - wz_cmp.imag)
@@ -106,14 +127,19 @@ def test_cerrf_q1():
     assert d_abs_im.max() < 0.5e-9
 
 
-def test_cerrf_all_quadrants():
+@pytest.mark.parametrize(
+    'context',
+    xo.context.get_test_contexts(),
+    ids=[str(ctx) for ctx in xo.context.get_test_contexts()],
+)
+def test_cerrf_all_quadrants(faddeeva_calculator, context):
+    FaddeevaCalculator = faddeeva_calculator
+
     x0 = 5.33
     y0 = 4.29
     num_args = 10000
 
-    assert xo.ContextCpu in available
-
-    ctx = xo.ContextCpu(omp_num_threads=0)
+    ctx = context
 
     re_max = np.float64(np.sqrt(2.0) * x0)
     im_max = np.float64(np.sqrt(2.0) * y0)
@@ -134,151 +160,20 @@ def test_cerrf_all_quadrants():
 
     re_absc = np.random.uniform(re_min, re_max, num_args)
     im_absc = np.random.uniform(im_min, im_max, num_args)
-    wz_cmp_re = np.arange(num_args, dtype=np.float64)
-    wz_cmp_im = np.arange(num_args, dtype=np.float64)
+    z = re_absc + 1j * im_absc
+
+    # Calculate the values based on the grid
+    calculator = FaddeevaCalculator(z=z, _context=context)
+    calculator.compute()
 
     # Create comparison data for veryfing the correctness of cerrf().
     # Cf. the comments about scipy's wofz implementation in test_cerrf_q1()
     # for details!
+    wz_cmp = wofz_scipy(z)
 
-    for ii, (x, y) in enumerate(zip(re_absc, im_absc)):
-        wz = wofz_scipy(x + 1.0j * y)
-        wz_cmp_re[ii] = wz.real
-        wz_cmp_im[ii] = wz.imag
-
-    src_code = """
-    /*gpukern*/ void eval_cerrf_all_quadrants(
-        const int n,
-        /*gpuglmem*/ double const* /*restrict*/ re,
-        /*gpuglmem*/ double const* /*restrict*/ im,
-        /*gpuglmem*/ double* /*restrict*/ wz_re,
-        /*gpuglmem*/ double* /*restrict*/ wz_im )
-    {
-        for(int tid = 0 ; tid < n ; ++tid ) { //vectorize_over tid n
-
-            if( tid < n )
-            {
-                double const x = re[ tid ];
-                double const y = im[ tid ];
-                double wz_x, wz_y;
-
-                cerrf( x, y, &wz_x, &wz_y );
-
-                wz_re[ tid ] = wz_x;
-                wz_im[ tid ] = wz_y;
-            }
-        }//end_vectorize
-    }
-    """
-
-    kernel_descriptions = {
-        "eval_cerrf_all_quadrants": xo.Kernel(
-            args=[
-                xo.Arg(xo.Int32, name="n"),
-                xo.Arg(xo.Float64, name="re", const=True, pointer=True),
-                xo.Arg(xo.Float64, name="im", const=True, pointer=True),
-                xo.Arg(xo.Float64, name="wz_re", pointer=True),
-                xo.Arg(xo.Float64, name="wz_im", pointer=True),
-            ],
-            n_threads="n",
-        ),
-    }
-
-    headers = [
-        _pkg_root.joinpath("headers/constants.h"),
-        _pkg_root.joinpath("headers/sincos.h"),
-        _pkg_root.joinpath("headers/power_n.h"),
-        _pkg_root.joinpath("fieldmaps/bigaussian_src/complex_error_function.h"),
-    ]
-
-    wz_re = np.arange(num_args, dtype=np.float64)
-    wz_im = np.arange(num_args, dtype=np.float64)
-
-    re_absc_dev = ctx.nparray_to_context_array(re_absc)
-    im_absc_dev = ctx.nparray_to_context_array(im_absc)
-    wz_re_dev = ctx.nparray_to_context_array(wz_re)
-    wz_im_dev = ctx.nparray_to_context_array(wz_im)
-
-    ctx.add_kernels(
-        sources=[src_code], kernels=kernel_descriptions, extra_headers=headers
-    )
-
-    ctx.kernels.eval_cerrf_all_quadrants(
-        n=num_args,
-        re=re_absc_dev,
-        im=im_absc_dev,
-        wz_re=wz_re_dev,
-        wz_im=wz_im_dev,
-    )
-
-    wz_re = ctx.nparray_from_context_array(wz_re_dev)
-    wz_im = ctx.nparray_from_context_array(wz_im_dev)
-
-    d_abs_re = np.fabs(wz_re - wz_cmp_re)
-    d_abs_im = np.fabs(wz_im - wz_cmp_im)
+    difference = calculator.w - wz_cmp
+    d_abs_re = np.fabs(difference.real)
+    d_abs_im = np.fabs(difference.imag)
 
     assert d_abs_re.max() < 0.5e-9
     assert d_abs_im.max() < 0.5e-9
-
-
-source = '''
-    /*gpukern*/ void FaddeevaCalculator_compute(FaddeevaCalculatorData data) {
-        int64_t len = FaddeevaCalculatorData_len_z_re(data);
-
-        for (int64_t ii = 0; ii < len; ii++) {  //vectorize_over ii len
-            double z_re = FaddeevaCalculatorData_get_z_re(data, ii);
-            double z_im = FaddeevaCalculatorData_get_z_im(data, ii);
-            double w_re, w_im;
-
-            cerrf(z_re, z_im, &w_re, &w_im);
-
-            FaddeevaCalculatorData_set_w_re(data, ii, w_re);
-            FaddeevaCalculatorData_set_w_im(data, ii, w_im);
-        } //end_vectorize
-    }
-'''
-
-class FaddeevaCalculator(xo.HybridClass):
-    _xofields = {
-        'z_re': xo.Float64[:],
-        'z_im': xo.Float64[:],
-        'w_re': xo.Float64[:],
-        'w_im': xo.Float64[:],
-    }
-
-    _extra_c_sources = [
-        _pkg_root.joinpath("headers/constants.h"),
-        _pkg_root.joinpath("headers/sincos.h"),
-        _pkg_root.joinpath("headers/power_n.h"),
-        _pkg_root.joinpath("fieldmaps/bigaussian_src/complex_error_function.h"),
-        source,
-    ]
-
-    _kernels = {
-        'FaddeevaCalculator_compute': xo.Kernel(
-            args=[
-                xo.Arg(xo.ThisClass, name='data'),
-            ],
-        )
-    }
-
-    def __init__(self, z, **kwargs):
-        z = np.array(z)
-
-        self.xoinitialize(
-            z_re = z.real,
-            z_im = z.imag,
-            w_re = len(z),
-            w_im = len(z),
-            **kwargs,
-        )
-
-    def compute(self):
-        self._xobject.compile_kernels(only_if_needed=True)
-        import ipdb; ipdb.set_trace()
-        self._context.kernels.FaddeevaCalculator_compute.description.n_threads = len(self.z_re)
-        self._context.kernels.FaddeevaCalculator_compute(data=self)
-
-context = xo.ContextCupy()
-fc = FaddeevaCalculator(z=[1+1j, 2+2j, 3+3j] * 256, _context=context)
-fc.compute()
