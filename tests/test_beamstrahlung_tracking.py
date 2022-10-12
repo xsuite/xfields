2a80111223620a6773b46abe235ebb1898b736fd
Peter Kicsiny <peter.kicsiny@cern.ch>
2022-10-12 17:12:23 +0200

    add test

--- tests/test_beamstrahlung_tracking.py
+++ tests/test_beamstrahlung_tracking.py
@@ -271,6 +271,10 @@ record = tracker.start_internal_logging_for_elements_of_type(xf.BeamBeamBiGaussi
 tracker.track(particles_b1, num_turns=n_turns)
 tracker.stop_internal_logging_for_elements_of_type(xf.BeamBeamBiGaussian3D)
 
+#########
+# Tests #
+#########
+
 # average and maximum BS parameter
 # https://www.researchgate.net/publication/2278298_Beam-Beam_Phenomena_In_Linear_Colliders
 # page 20
@@ -291,98 +295,12 @@ print("Y avg. ratio [1]:", upsilon_avg_sim / upsilon_avg)
 assert np.abs(upsilon_max_sim / upsilon_max - 1) < 1e-2
 assert np.abs(upsilon_avg_sim / upsilon_avg - 1) < 5e-2
 
-#########
-# plots #
-#########
-def stat_emittance_from_monitor(emits_dict, n_macroparticles, n_turns, alpha_x=0, alpha_y=0, beta_x=0, beta_y=0):
-    """
-    compute statistical emittances. First normalize coordinates by using (263) then (130) from
-    https://arxiv.org/pdf/2107.02614.pdf
-    """
-        
-    x     = np.reshape(emits_dict["x"],     (n_macroparticles, n_turns))
-    px    = np.reshape(emits_dict["px"],    (n_macroparticles, n_turns))
-    y     = np.reshape(emits_dict["y"],     (n_macroparticles, n_turns))
-    py    = np.reshape(emits_dict["py"],    (n_macroparticles, n_turns))
-    z     = np.reshape(emits_dict["zeta"],  (n_macroparticles, n_turns))
-    delta = np.reshape(emits_dict["delta"], (n_macroparticles, n_turns))
-        
-    x_norm     = x / np.sqrt(beta_x)
-    y_norm     = y / np.sqrt(beta_y)
-    px_norm    = alpha_x / beta_x * x + beta_x * px
-    py_norm    = alpha_y / beta_y * y + beta_y * py
-    
-    emit_x = np.sqrt(np.mean(( x_norm -  np.mean(x_norm, axis=0))**2, axis=0) *\
-                     np.mean((px_norm - np.mean(px_norm, axis=0))**2, axis=0) -\
-                     np.mean(( x_norm -  np.mean(x_norm, axis=0)) *\
-                             (px_norm - np.mean(px_norm, axis=0)), axis=0)**2)
-        
-    emit_y = np.sqrt(np.mean(( y_norm -  np.mean(y_norm, axis=0))**2, axis=0) *\
-                     np.mean((py_norm - np.mean(py_norm, axis=0))**2, axis=0) -\
-                     np.mean(( y_norm -  np.mean(y_norm, axis=0)) *\
-                             (py_norm - np.mean(py_norm, axis=0)), axis=0)**2)
-        
-    emit_s = np.sqrt(np.mean((    z - np.mean(    z, axis=0))**2, axis=0) *\
-                     np.mean((delta - np.mean(delta, axis=0))**2, axis=0) -\
-                     np.mean((    z - np.mean(    z, axis=0)) *\
-                             (delta - np.mean(delta, axis=0)), axis=0)**2)
-        
-    return emit_x, emit_y, emit_s
-    
 # get emittances and RMS beam sizes
 coords_dict = monitor_coords.to_dict()["data"]
-emits_dict  = monitor_emits.to_dict()["data"]
-
-emit_x_arr, emit_y_arr, emit_s_arr = stat_emittance_from_monitor(emits_dict, n_macroparticles_b1, n_turns,
-                            alpha_x=alpha_x_sext_right,
-                            alpha_y=alpha_y_sext_right,
-                            beta_x=beta_x_sext_right,
-                            beta_y=beta_y_sext_right)
-
-x_std_arr = np.std(np.reshape(coords_dict["x"],    (n_macroparticles_b1, n_turns)), axis=0)
-y_std_arr = np.std(np.reshape(coords_dict["y"],    (n_macroparticles_b1, n_turns)), axis=0)
 z_std_arr = np.std(np.reshape(coords_dict["zeta"], (n_macroparticles_b1, n_turns)), axis=0)
-
-# bunch size evolution
-turns_arr = np.arange(n_turns)
 w = 500
 
 print("sigma_z blowup [1]:", np.mean(z_std_arr[-w:])/sigma_z_tot)
 
 assert np.abs(np.mean(z_std_arr[-w:])/sigma_z_tot -1) < 5e-2
 
-fig, ax = plt.subplots(3,2, figsize=(20,10))
-
-ax[0,0].plot(turns_arr, x_std_arr, label="{:.2f}".format(np.mean(x_std_arr[-w:])/sigma_x))
-ax[1,0].plot(turns_arr, y_std_arr, label="{:.2f}".format(np.mean(y_std_arr[-w:])/sigma_y))
-ax[2,0].plot(turns_arr, z_std_arr, label="{:.2f}".format(np.mean(z_std_arr[-w:])/sigma_z_tot))
-
-ax[0,1].plot(turns_arr, emit_x_arr, label="{:.2f}".format(np.mean(emit_x_arr[-w:])/physemit_x))
-ax[1,1].plot(turns_arr, emit_y_arr, label="{:.2f}".format(np.mean(emit_y_arr[-w:])/physemit_y))
-ax[2,1].plot(turns_arr, emit_s_arr, label="{:.2f}".format(np.mean(emit_s_arr[-w:])/physemit_s_tot))
-
-ax[0,0].axhline(sigma_x, c="r")
-ax[1,0].axhline(sigma_y, c="r")
-ax[2,0].axhline(sigma_z_tot, c="r")
-
-ax[0,1].axhline(physemit_x, c="r")
-ax[1,1].axhline(physemit_y, c="r")
-ax[2,1].axhline(physemit_s_tot, c="r")
-
-ax[0,0].legend()
-ax[1,0].legend()
-ax[2,0].legend()
-ax[0,1].legend()
-ax[1,1].legend()
-ax[2,1].legend()
-
-plt.show()
-
-# beamstrahlung photon spectrum
-fig, ax = plt.subplots(1,1,figsize=(12,8))
-ax.hist(record.beamstrahlungtable.photon_energy/1e9, bins=np.logspace(np.log10(1e-14), np.log10(1e1), 100), histtype="step");
-ax.set_xscale("log")
-ax.set_yscale("log")
-ax.set_xlabel("E [GeV]")
-ax.set_ylabel("Count [1]")
-plt.show()
