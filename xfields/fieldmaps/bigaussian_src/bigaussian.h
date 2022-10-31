7d9287ed8dc4c129b5b160337593a27611df57c6
Szymon Łopaciuk <szymon@lopaciuk.eu>
2022-10-31 16:42:09 +0100

    create a separate file for `compute_gx_gy` in `bigaussian_src`
    Signed-off-by: Szymon Łopaciuk <szymon@lopaciuk.eu>

--- xfields/fieldmaps/bigaussian_src/bigaussian.h
+++ xfields/fieldmaps/bigaussian_src/bigaussian.h
@@ -9,6 +9,9 @@
 // for quick test with gcc
 #include "constants.h" //only_for_context none
 #include "faddeeva.h" //only_for_context none
+#include "complex_error_function.h" //only_for_context none
+#include "compute_gx_gy.h" //only_for_context none
+//include_file compute_gx_gy.h for_context cpu_serial opencl cuda cpu_openmp
 
 /*gpufun*/
 void get_transv_field_gauss_round(
@@ -124,49 +127,6 @@ void get_Ex_Ey_gauss(
 	            sigma_x, sigma_y, 0., 0., x, y, Ex_ptr, Ey_ptr);
 
 	}
-}
-
-
-/*gpufun*/ void compute_Gx_Gy(
-        const double  x,
-        const double  y,
-        const double  sigma_x,
-        const double  sigma_y,
-        const double  min_sigma_diff,
-        const double  Ex,
-        const double  Ey,
-        double* Gx_ptr,
-        double* Gy_ptr){
-
-    double Gx, Gy;
-
-    if (fabs(sigma_x-sigma_y) < min_sigma_diff){
-        const double sigma = 0.5*(sigma_x+sigma_y);
-	      if ((x*x+y*y)<1e-14){
-            Gx = 1./(8*PI*EPSILON_0*sigma*sigma);
-	          Gy = Gx;
-	      }
-	      else{
-            Gx = 1/(2.*(x*x+y*y))*(y*Ey-x*Ex+1./(2*PI*EPSILON_0*sigma*sigma)
-                                *x*x*exp(-(x*x+y*y)/(2.*sigma*sigma)));
-            Gy = 1./(2*(x*x+y*y))*(x*Ex-y*Ey+1./(2*PI*EPSILON_0*sigma*sigma)
-                                *y*y*exp(-(x*x+y*y)/(2.*sigma*sigma)));
-	       }
-    }
-    else{
-
-        const double Sig_11 = sigma_x*sigma_x;
-        const double Sig_33 = sigma_y*sigma_y;
-
-        Gx =-1./(2*(Sig_11-Sig_33))*(x*Ex+y*Ey+1./(2*PI*EPSILON_0)
-                   *(sigma_y/sigma_x*exp(-x*x/(2*Sig_11)-y*y/(2*Sig_33))-1.));
-        Gy =1./(2*(Sig_11-Sig_33))*(x*Ex+y*Ey+1./(2*PI*EPSILON_0)*
-                      (sigma_x/sigma_y*exp(-x*x/(2*Sig_11)-y*y/(2*Sig_33))-1.));
-
-    }
-
-    *Gx_ptr = Gx;
-    *Gy_ptr = Gy;
 }
 
 #endif
