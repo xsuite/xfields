675d0df868d02bdeb0e2126d22eb5dd5397617e6
giadarol <giovanni.iadarola@cern.ch>
2022-12-31 15:25:13 -0300

    Plotting normalized separations

--- examples/005_beam_beam_configuration/000_configure_beam_beam.py
+++ examples/005_beam_beam_configuration/000_configure_beam_beam.py
@@ -1,4 +1,6 @@
 import json
+
+import numpy as np
 import xtrack as xt
 import xfields as xf
 
@@ -44,13 +46,15 @@ bb_df_b2 = bb_df_b2_ret[keep_columns].copy()
 configure_beam_beam_elements(bb_df_b1, bb_df_b2, tracker_b1, tracker_b4,
                                  nemitt_x, nemitt_y, crab_strong_beam, ip_names)
 
-tracker, beam_name = (tracker_b1, 'b1')
-ip = 5
+tracker, tracker_other, beam_name = (tracker_b1, tracker_b4, 'b1')
+ip = 1
 
 line = tracker.line
 bb_ele_names = []
 x_strong =[]
 x_weak = []
+y_strong =[]
+y_weak = []
 
 for nn in line.element_names:
     ee = line[nn]
@@ -58,22 +62,46 @@ for nn in line.element_names:
         if f'{ip}{beam_name}' in nn:
             bb_ele_names.append(nn)
             x_weak.append(ee.ref_shift_x)
+            y_weak.append(ee.ref_shift_y)
             x_strong.append(ee.other_beam_shift_x + ee.ref_shift_x)
+            y_strong.append(ee.other_beam_shift_y + ee.ref_shift_y)
 
 
 import matplotlib.pyplot as plt
-plt.close('all')
 
 tw = tracker.twiss()
-
+tw_other = tracker_other.twiss(reverse=True)
 tw_df = tw.to_pandas()
 tw_df.set_index('name', inplace=True)
 
+beta0gamma0 = tw.particle_on_co.beta0[0] * tw.particle_on_co.gamma0[0]
 s_bb = tw_df.loc[bb_ele_names, 's'].values
+betx_bb = tw_df.loc[bb_ele_names, 'betx'].values
+bety_bb = tw_df.loc[bb_ele_names, 'bety'].values
 
-plt.plot(s_bb, x_weak, 'o-')
-plt.plot(s_bb, x_strong, 'o-')
-plt.plot(tw.s, tw.x, '-')
+plt.close('all')
+plt.figure(1, figsize=(6.4*1.4, 4.8*1.2))
+spx = plt.subplot(2, 2, 1)
+spx.plot(s_bb, x_weak, 'o-')
+spx.plot(s_bb, x_strong, 'o-')
+spx.plot(tw.s, tw.x, '-')
+spx.plot(tw_other.s, tw_other.x, '-')
+
+spy = plt.subplot(2, 2, 2, sharex=spx, sharey=spx)
+spy.plot(s_bb, y_weak, 'o-')
+spy.plot(s_bb, y_strong, 'o-')
+spy.plot(tw.s, tw.y, '-')
+spy.plot(tw_other.s, tw_other.y, '-')
+
+spnx = plt.subplot(2, 2, 3, sharex=spx)
+spnx.plot(s_bb, x_weak/np.sqrt(betx_bb*nemitt_x/beta0gamma0), 'o-')
+spnx.plot(s_bb, x_strong/np.sqrt(betx_bb*nemitt_x/beta0gamma0), 'o-')
+
+spny = plt.subplot(2, 2, 4, sharex=spx, sharey=spnx)
+spny.plot(s_bb, y_weak/np.sqrt(bety_bb*nemitt_y/beta0gamma0), 'o-')
+spny.plot(s_bb, y_strong/np.sqrt(bety_bb*nemitt_y/beta0gamma0), 'o-')
+
+spx.set_xlim(s_bb[0], s_bb[-1])
 
 plt.show()
 
