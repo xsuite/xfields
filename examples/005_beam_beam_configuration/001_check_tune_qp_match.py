067ff1ebac51c8a2ecae08aed12f572a18a8e44b
giadarol <giovanni.iadarola@cern.ch>
2023-01-02 16:47:30 -0300

    Introduced tolerances

--- examples/005_beam_beam_configuration/001_check_tune_qp_match.py
+++ examples/005_beam_beam_configuration/001_check_tune_qp_match.py
@@ -9,13 +9,18 @@ line_b1 = xt.Line.from_dict(dct_b1)
 
 tracker = line_b1.build_tracker()
 
+target_qx = 62.315
+target_qy = 60.315
+
 tw = tracker.twiss()
-
+print('Match tune')
 tracker.match(vary=['kqtf.b1', 'kqtd.b1'],
-    targets = [('qx', 62.315), ('qy', 60.325)])
+    targets = [('qx', target_qx, 1e-4), ('qy', target_qy, 1e-4)])
 
+print('Match chromaticity')
 tracker.match(vary=['ksf.b1', 'ksd.b1',],
-    targets = [('dqx', 15), ('dqy', 15)])
+    targets = [('dqx', 15, .1), ('dqy', 15, .1)])
 
+print('Match tune and chromaticity')
 tracker.match(vary=['kqtf.b1', 'kqtd.b1', 'ksf.b1', 'ksd.b1'],
-    targets = [('qx', 62.315), ('qy', 60.325), ('dqx', 15), ('dqy', 15)])
+    targets = [('qx', 62.315, 1e-4), ('qy', 60.325, 1e-4), ('dqx', 15, .1), ('dqy', 15, .1)])
