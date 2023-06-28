4df0f893ab2b93d1d1af3438fa8ee4fbc89b1214
giadarol <giovanni.iadarola@cern.ch>
2023-06-28 15:39:00 +0200

    Remove full comment

--- xfields/headers/beamstrahlung_spectrum.h
+++ xfields/headers/beamstrahlung_spectrum.h
@@ -27,7 +27,7 @@ int beamstrahlung_0(LocalParticle *part,
     const double g2_c[4] = {0.2847316689, 0.58306846, 0.3915531539, 1.0};
 
     double c1 = 1.5*HBAR_GEVS / pow(MELECTRON_GEV, 3.0) * C_LIGHT;  // [c^4/Gev^2] 2.22e-6 = 1.5*hbar*cst.c/e0**3
-    double xcrit = c1 * pow(energy*1e-9, 2.0) * rho_inv;  // [1] ecrit/E magnitude of quantum correction, in guineapig: xcrit (C) = xi (doc) = upsbar (C++)
+    double xcrit = c1 * pow(energy*1e-9, 2.0) * rho_inv;
     (*ecrit) = xcrit * energy*1e-9;  // [GeV]
     //double omega_crit = (*ecrit)/HBAR_GEVS;  // [1/s] = 1.5 * gamma**3 * cst.c / rho
     //double upsilon = 2.0/3.0 * (*ecrit) / (energy*1e-9);  // [1] beamstrahlung parameter for single macropart
