15663dff32ccb7c2fdb337086cf5814f984f72d4
giadarol <giovanni.iadarola@cern.ch>
2023-09-04 22:20:09 +0200

    remove header

--- xfields/beam_elements/electroncloud.py
+++ xfields/beam_elements/electroncloud.py
@@ -60,7 +60,6 @@ class ElectronCloud(xt.BeamElement):
 
     _extra_c_sources = [
         _pkg_root.joinpath('headers','particle_states.h'),
-        xt.general._pkg_root.joinpath('headers/atomicadd.h'),
         _pkg_root.joinpath('fieldmaps/interpolated_src/tricubic_coefficients.h'),
         _pkg_root.joinpath('fieldmaps/interpolated_src/cubic_interpolators.h'),
         _pkg_root.joinpath('beam_elements/electroncloud_src/electroncloud.h'),
