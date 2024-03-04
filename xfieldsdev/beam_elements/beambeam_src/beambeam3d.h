3855b4e81728429619d4fa707407b64e1f766fd6
Chenying Zhang <chenyingcz@gmail.com>
2024-03-04 14:55:03 +0100

    fixing the calling of lumicalc

--- xfieldsdev/beam_elements/beambeam_src/beambeam3d.h
+++ xfieldsdev/beam_elements/beambeam_src/beambeam3d.h
@@ -157,7 +157,7 @@ void synchrobeam_kick(
     if (flag_combilumi == 1){
 
         // gaussian charge density: at x, y density given by the 2D gaussian, local lumi depending on x y, total lumi sum of all
-        double combilumi = lumicalc(gsl_histogram2d* h1,gsl_histogram2d* h2,double intensity1,double intensity2,double frev);
+        double combilumi = lumicalc(h1,h2,intensity1,intensity2,frev);
         // init record table
         BeamBeamBiGaussian3DRecordData combilumi_record = NULL;
         CombiLumiTableData combilumi_table                   = NULL;
