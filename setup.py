8d95dc0c2ac77342d96ee76c27ac3d2e0258f846
Chenying Zhang <chenyingcz@gmail.com>
2024-04-04 13:44:40 +0200

    deleted gsl inclusion

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ from pathlib import Path
 #######################################
 # Prepare list of compiled extensions #
 #######################################
-gsl= Extension('gsl',
-                    include_dirs=['/opt/homebrew/Cellar/gsl/2.7.1/include'],
-                    libraries=['gsl', 'gslcblas'],
-                    library_dirs=['/opt/homebrew/Cellar/gsl/2.7.1/lib']
-                    )
-
 
 extensions = []
 
