09aca47f8a12cdd2dc755ac19bdd076739bd8094
Felix Soubelet <felix.soubelet@protonmail.com>
2024-04-11 10:46:59 +0200

    example of analytical growth rates with Dy

--- examples/005_ibs/001_growth_rates_from_parameters_w_vdisp.py
+++ examples/005_ibs/001_growth_rates_from_parameters_w_vdisp.py
@@ -2,6 +2,9 @@
 # This file is part of the Xfields Package.   #
 # Copyright (c) CERN, 2021.                   #
 # ########################################### #
+import json
+
+import numpy as np
 import xtrack as xt
 
 from xfields.ibs import get_intrabeam_scattering_growth_rates
@@ -11,19 +14,30 @@ from xfields.ibs import get_intrabeam_scattering_growth_rates
 ##########################
 
 # TODO: have lines or something in this repo?
-fname_line_particles = "../../../xtrack/test_data/sps_ions/line_and_particle.json"
+fname_line_particles = "../../../xtrack/test_data/lhc_no_bb/line_and_particle.json"
+
+with open(fname_line_particles, "r") as fid:
+    input_data = json.load(fid)
+
+# line = xt.Line.from_dict(input_data["line"])
 line = xt.Line.from_json(fname_line_particles)
+line.particle_ref = xt.Particles.from_dict(input_data["particle"])
+
+twiss = line.twiss(method="4d")
+if np.count_nonzero(twiss.dy) > 0:
+    print()
+    print("There is vertical dispersion, Nagaitsev will be wrong in vertical")
 
 #####################
 # Define parameters #
 #####################
 
-# Line is for SPS ions at injection
-bunch_intensity: int = int(3.5e8)
-nemit_x: float = 1.2612e-6
-nemit_y: float = 0.9081e-6
-sigma_delta: float = 3.59e-4
-bunch_length: float = 19.51e-2
+# Line is for LHC protons at top energy
+bunch_intensity: int = int(1.8e11)
+nemit_x: float = 1.8e-6
+nemit_y: float = 1.8e-6
+sigma_delta: float = 4.71e-5
+bunch_length: float = 3.75e-2
 
 ###################################
 # Get growth rates with Nagaitsev #
@@ -57,6 +71,7 @@ bm_growth_rates = get_intrabeam_scattering_growth_rates(
     normalized_emittances=True,
 )
 
+
 ##########################################################
 # Compare: we expect Nagaitsev to be wrong in horizontal #
 ##########################################################
@@ -71,9 +86,8 @@ print(f"Bjorken-Mtingwa: {bm_growth_rates}")
 # Define parameters #
 #####################
 
-# Should roughly be equivalent
-geom_epsx: float = 1.742e-7
-geom_epsy: float = 1.254e-7
+geom_epsx: float = 2.598e-10
+geom_epsy: float = 2.598e-10
 
 ###################################
 # Get growth rates with Nagaitsev #
