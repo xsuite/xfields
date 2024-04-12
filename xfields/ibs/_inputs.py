2ad01b50f090aa9c8600c0d1225752e0d7d3820d
Felix Soubelet <felix.soubelet@protonmail.com>
2024-04-12 14:27:05 +0200

    forget the commented out stuff, and let's now do one single class

--- xfields/ibs/_inputs.py
+++ xfields/ibs/_inputs.py
@@ -15,7 +15,6 @@ LOGGER = getLogger(__name__)
 # Re-implemented BeamParameters and OpticsParameters as xo.HybridClass objects,
 # and got rid of the parts initializing from MAD-X.
 
-
 class BeamParameters(xo.HybridClass):
     """Holds relevant beam parameters for IBS calculations.
 
@@ -38,13 +37,13 @@ class BeamParameters(xo.HybridClass):
     """
 
     _xofields = {
-        "n_part": xo.Int64,
-        "particle_charge": xo.Int64,
-        "particle_mass_eV": xo.Float64,
-        "total_energy_eV": xo.Float64,
-        "gamma_rel": xo.Float64,
-        "beta_rel": xo.Float64,
-        "particle_classical_radius_m": xo.Float64,
+        "n_part": xo.Int64,  # num_particles
+        "particle_charge": xo.Int64,  # q0
+        "particle_mass_eV": xo.Float64,  # mass0
+        "total_energy_eV": xo.Float64,  # energy
+        "gamma_rel": xo.Float64,  # gamma0
+        "beta_rel": xo.Float64,  # beta0
+        "particle_classical_radius_m": xo.Float64,  # classical_particle_radius0
     }
 
     def __init__(self, particles: xt.Particles) -> None:
@@ -191,40 +190,118 @@ class OpticsParameters(xo.HybridClass):
         return cls(line, **kwargs)
 
 
-# ----- Helper functions ----- #
 
-# TODO (Gianni): determine if we want this - wait for refactor of TwissTable behaviour?
-# def _is_twiss_centered(twiss: xt.TwissTable) -> bool:
-#     r"""
-#     Determines if the Twiss was performed at the center of elements.
+# TODO: Just one class for inputs
+# Takes a TwissTable (but has a .from_line method)
+# Is basically the Opticsparameters and uses the tw.particle_on_co for the attrivutes that BeamParams uses
+# Still will need user to give npart if not giving a Particles objects
 
-#     .. hint::
-#         This check is taken from the Fortran source code of the ``IBS`` module in ``MAD-X``.
-#         We skip all rows in the table until we get to the first element with non-zero length,
-#         take note of its `s` position, then the `s` and `l` of the next element. We compare the
-#         :math:`\Delta s` to the length and conclude. If the two match, then the :math:`\Delta s`
-#         is exactly the length of the second element which means the `s` values are given at the
-#         end of elements, and therefore we are not centered. Otherwise, we are.
+# class IBSInputs(xo.HybridClass):
+#     """Holds relevant beam parameters and optics for IBS calculations.
 
-#     Parameters
+#     Attributes
 #     ----------
-#     twiss : xt.TwissTable
-#         The result of a Twiss call on the `xt.Line`.
-
-#     Returns
-#     -------
-#     bool
-#         Returns `True` if the Twiss is centered, `False` otherwise.
+#     n_part : int
+#         Number of simulated particles.
+#     particle_charge : int
+#         Elementary particle charge, in # of Coulomb charges.
+#     particle_mass_eV : float
+#         Particle mass in [eV].
+#     total_energy_eV : float
+#         Total energy of the simulated particles in [eV].
+#     gamma_rel : float
+#         Relativistic gamma of the simulated particles.
+#     beta_rel : float
+#         Relativistic gamma of the simulated particles.
+#     particle_classical_radius_m : float
+#         The particles' classical radius in [m].
+#     s : ArrayLike
+#         Longitudinal positions of the machine elements in [m].
+#     circumference : float
+#         Machine circumference in [m].
+#     slip_factor : float
+#         Slip factor of the machine (positive above transition).
+#     revolution_frequency : float
+#         Revolution frequency of the machine in [Hz].
+#     betx : ArrayLike
+#         Horizontal beta functions in [m].
+#     bety : ArrayLike
+#         Vertical beta functions in [m].
+#     alfx : ArrayLike
+#         Horizontal alpha functions.
+#     alfy : ArrayLike
+#         Vertical alpha functions.
+#     dx : ArrayLike
+#         Horizontal dispersion functions in [m].
+#     dy : ArrayLike
+#         Vertical dispersion functions in [m].
+#     dpx : ArrayLike
+#         Horizontal dispersion of px (d px / d delta).
+#     dpy : ArrayLike
+#         Vertical dispersion of py (d px / d delta).
 #     """
-#     df = twiss.to_pandas()
-#     # Get to the first row with an actual element of non-zero length
-#     tw = df[df.l != 0]
-#     # Get the "first" value of s and l
-#     s0 = tw.s.to_numpy()[0]
-#     # Get the s variable at the next element
-#     l1 = tw.l.to_numpy()[1]
-#     s1 = tw.s.to_numpy()[1]
-#     # Compare s1 - s0 with the length of the second element, if it matches then the delta_s corresponds
-#     # to the length of the element which means we are getting values at the exit of the elements,
-#     # aka the twiss is not centered
-#     return not np.isclose(s1 - s0, l1)
+#     _xofields = {
+#         "s": xo.Float64[:],
+#         "circumference": xo.Float64,
+#         "slip_factor": xo.Float64,
+#         "revolution_frequency": xo.Float64,
+#         "betx": xo.Float64[:],
+#         "bety": xo.Float64[:],
+#         "alfx": xo.Float64[:],
+#         "alfy": xo.Float64[:],
+#         "dx": xo.Float64[:],
+#         "dy": xo.Float64[:],
+#         "dpx": xo.Float64[:],
+#         "dpy": xo.Float64[:],
+#     }
+
+
+#     def __init__(self, twiss: xt.TwissTable) -> None:
+#         """
+#         Init by providing the xt.TwissTable object corresponding to a specific
+#         line configuration.
+
+#         Parameters
+#         ----------
+#         line : xtrack.TwissTable
+#             Twiss results of the `xtrack.Line` configuration.
+#         """
+#         # First, a check for the coupling situation in the machine
+#         if not np.isclose(twiss.c_minus, 0, atol=5e-4):  # there is "some" betatron coupling
+#             LOGGER.warning(
+#                 f"There is betatron coupling in the machine (|Cminus| = {twiss.c_minus:.3e}), "
+#                 "which is not taken into account in analytical calculations."
+#             )
+
+#         s = twiss.s
+#         circumference = twiss.circumference
+#         slip_factor = twiss.slip_factor
+#         revolution_frequency = twiss.beta0 * c / circumference
+#         betx = twiss.betx
+#         bety = twiss.bety
+#         alfx = twiss.alfx
+#         alfy = twiss.alfy
+#         dx = twiss.dx
+#         dy = twiss.dy
+#         dpx = twiss.dpx
+#         dpy = twiss.dpy
+
+#         self.xoinitialize(
+#             s=s,
+#             circumference=circumference,
+#             slip_factor=slip_factor,
+#             revolution_frequency=revolution_frequency,
+#             betx=betx,
+#             bety=bety,
+#             alfx=alfx,
+#             alfy=alfy,
+#             dx=dx,
+#             dy=dy,
+#             dpx=dpx,
+#             dpy=dpy,
+#         )
+
+#     @classmethod
+#     def from_line(cls, line: xt.Line, **kwargs) -> Self:  # noqa: F821
+#         """Only here so that OpticsParameters.from_line() is also possible."""
+#         return cls(line, **kwargs)
