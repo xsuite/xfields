c22bb36011842ff6406eab2193f2eefc229d410d
Xavier Buffat <xavier.buffat@cern.ch>
2024-04-15 15:42:42 +0200

    Fix buf with parsing of wake components

--- xfields/beam_elements/wakefield.py
+++ xfields/beam_elements/wakefield.py
@@ -118,8 +118,9 @@ class MultiWakefield:
                 else:
                     tokens = wake_component.split('_')
                     coord_target = tokens[1][0]
+                    print(tokens,len(tokens[1]))
                     if len(tokens[1]) == 2:
-                        coord_source = tokens[1][0]
+                        coord_source = tokens[1][1]
                     else:
                         coord_source = coord_target
                     kick = 'p'+coord_target
@@ -127,7 +128,6 @@ class MultiWakefield:
                         source_moments.append(coord_source)
                     elif tokens[0] == 'quadrupole':
                         scale_kick = coord_source
-                    
                 wake_strength = -1E15 * wake_data[:,iwake_component]
                 wakefield = xf.Wakefield(
                     source_moments=source_moments,
