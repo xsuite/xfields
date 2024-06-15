46fa2fc0c5c1b9421b7ff145109a2e76d23f3e69
giadarol <giovanni.iadarola@cern.ch>
2024-06-15 06:05:10 -0700

    Clean up Log API

---
+++ examples/slicer/003_multilog.py
@@ -0,0 +1,23 @@
+import xtrack as xt
+import xpart as xp
+import xfields as xf
+
+import numpy as np
+
+line = xt.Line.from_json(
+    '../../../xtrack/test_data/hllhc15_thick/lhc_thick_with_knobs.json')
+line.build_tracker()
+
+line.vars['vrf400'] = 16
+
+class MyLog:
+    def __init__(self):
+        self._store = ['a', 'b']
+
+    def __call__(self, line, particle):
+        return {'a': 1, 'b': 2}
+
+p = line.build_particles(x=0)
+
+line.enable_time_dependent_vars = True
+line.track(p, num_turns=10, log=xt.Log(moments=MyLog()))
