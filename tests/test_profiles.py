3a9b2b10522516675f562f3ec00b3fe35521139e
elias <elias.waagaard@gmail.com>
2024-06-18 14:46:12 +0200

    test to check derivatives

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -34,3 +34,32 @@ def test_qgauss(test_context):
         assert np.isclose(area, npart)
         assert np.isclose(z_mean, z0)
         assert np.isclose(z_std, sigma_z)
+
+@for_all_test_contexts
+def test_qgauss_derivative(test_context):
+    z0 = 0.1
+    sigma_z = 0.2
+    npart = 1e11
+
+    for qq in [0, 0.5, 0.95, 1.05, 1.3]:
+        lprofile = LongitudinalProfileQGaussian(
+                _context=test_context,
+                number_of_particles=npart,
+                sigma_z=sigma_z,
+                z0=z0,
+                q_parameter=qq)
+
+        # Get line density
+        z = np.linspace(-10., 10., 10000)
+        z_dev = test_context.nparray_to_context_array(z)
+        lden_dev = lprofile.line_density(z_dev)
+        lden = test_context.nparray_from_context_array(lden_dev)
+
+        # Then get computed derivative of line density
+        lderivative_dev = lprofile.line_derivative(z_dev)
+        lderivative = test_context.nparray_from_context_array(lderivative_dev)
+
+        # Compare derivatives
+        finite_difference_derivative = np.diff(lden)
+
+        assert np.isclose(lderivative, finite_difference_derivative)
