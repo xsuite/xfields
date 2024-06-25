38aae6b43614b2dfac9543bdf0c726d4abcec004
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-25 11:54:48 +0200

    updated plot labels

--- examples/longitudinal_kick_in_spacecharge/004_qgaussian_derivative.py
+++ examples/longitudinal_kick_in_spacecharge/004_qgaussian_derivative.py
@@ -8,6 +8,19 @@ from xfields import LongitudinalProfileQGaussian
 import xobjects as xo
 import matplotlib.pyplot as plt
 
+plt.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 20,
+        "axes.titlesize": 20,
+        "axes.labelsize": 18,
+        "xtick.labelsize": 18,
+        "ytick.labelsize": 18,
+        "legend.fontsize": 15,
+        "figure.titlesize": 20,
+    }
+)
+
 # Longitudinal example parameters
 z0 = 0.1
 sigma_z = 0.2
@@ -40,10 +53,11 @@ for qq in [0.0, 0.5, 0.95, 1.05, 1.3]:
 
     print('q = {:.2f} max relative difference anal. vs num. : {:.3e}'.format(qq, max((lderivative - numerical_derivative) / numerical_derivative)))
 
-    fig, ax = plt.subplots(1, 1, figsize=(8,6))
-    ax.plot(z, lderivative, label='Derivative of function')
-    ax.plot(z, numerical_derivative, ls='--', label='Numerical derivative np')
-    ax.text(0.87, 0.92, 'q={:.2f}'.format(qq), color='k', fontsize=13.5, transform=ax.transAxes)
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.plot(z, lderivative, label='Analytical derivative')
+    ax.plot(z, numerical_derivative, ls='--', label='Numerical derivative')
+    ax.text(0.83, 0.92, 'q={:.2f}'.format(qq), color='k', fontsize=15.5, transform=ax.transAxes)
+    ax.set_ylabel('Function value')
     ax.set_xlabel('Zeta [m]')
-    ax.legend(loc='lower right')
+    ax.legend(loc='lower left')
     plt.show()
