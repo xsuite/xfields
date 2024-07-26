bf30b998cf96fce21e3dfd920d2072e7d64e42db
giadarol <giovanni.iadarola@cern.ch>
2024-07-26 14:38:35 +0200

    Clean up

--- examples/002_beambeam/011e_multiple_kicks_multiple_turns.py
+++ examples/002_beambeam/011e_multiple_kicks_multiple_turns.py
@@ -112,9 +112,7 @@ multitracker.track(num_turns=2)
 z_centroids, z_cuts, num_part_per_slice = constant_charge_slicing_gaussian(
                                 bunch_intensity, sigma_z, num_slices)
 z_centroids_from_tail = z_centroids[::-1]
-bbg_b1_ip1 = xf.BeamBeamBiGaussian3D(
-    phi=phi,
-    alpha=alpha,
+common_hirata_kwargs_b1 = dict(
     other_beam_q0=1.,
     slices_other_beam_num_particles=num_part_per_slice,
     slices_other_beam_zeta_center=z_centroids_from_tail,
@@ -123,22 +121,13 @@ bbg_b1_ip1 = xf.BeamBeamBiGaussian3D(
     slices_other_beam_Sigma_22=cov.Sigma22[0],
     slices_other_beam_Sigma_33=cov.Sigma33[0],
     slices_other_beam_Sigma_34=cov.Sigma34[0],
-    slices_other_beam_Sigma_44=cov.Sigma44[0],
-)
+    slices_other_beam_Sigma_44=cov.Sigma44[0])
 
-bbg_b1_ip2 = xf.BeamBeamBiGaussian3D(
-    phi=-1.2*phi,
-    alpha=1.3*alpha,
-    other_beam_q0=1.,
-    slices_other_beam_num_particles=num_part_per_slice,
-    slices_other_beam_zeta_center=z_centroids_from_tail,
-    slices_other_beam_Sigma_11=cov.Sigma11[0],
-    slices_other_beam_Sigma_12=cov.Sigma12[0],
-    slices_other_beam_Sigma_22=cov.Sigma22[0],
-    slices_other_beam_Sigma_33=cov.Sigma33[0],
-    slices_other_beam_Sigma_34=cov.Sigma34[0],
-    slices_other_beam_Sigma_44=cov.Sigma44[0],
-)
+bbg_b1_ip1 = xf.BeamBeamBiGaussian3D(phi=phi, alpha=alpha,
+                                     **common_hirata_kwargs_b1)
+
+bbg_b1_ip2 = xf.BeamBeamBiGaussian3D(phi=-1.2*phi, alpha=1.3*alpha,
+                                     **common_hirata_kwargs_b1)
 
 p_bbg = p_test.copy()
 bbg_b1_ip1.track(p_bbg)
