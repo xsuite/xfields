be2709c79bcc941fa2d48d30a43f4e0d95028cdc
kparasch <konstantinos.paraschou@cern.ch>
2024-11-22 14:29:25 +0100

    Add test for the configuration tools of electron cloud

--- tests/test_electroncloud.py
+++ tests/test_electroncloud.py
@@ -4,12 +4,19 @@
 # ########################################### #
 
 import numpy as np
+import json
+import pathlib
 from numpy.random import default_rng
+
 import xobjects as xo
 import xpart as xp
 import xfields as xf
+import xtrack as xt
 
 from xobjects.test_helpers import for_all_test_contexts
+
+XTRACK_TEST_DATA = pathlib.Path(__file__).parent.parent.parent / "xtrack" / "test_data/"
+
 
 
 @for_all_test_contexts
@@ -102,3 +109,33 @@ def test_tricubic_interpolation(test_context):
     assert np.allclose(part.px[mask_p], true_px, atol=1.e-13, rtol=1.e-13)
     assert np.allclose(part.py[mask_p], true_py, atol=1.e-13, rtol=1.e-13)
     assert np.allclose(part.pzeta[mask_p], true_pzeta, atol=1.e-13, rtol=1.e-13)
+
+@for_all_test_contexts
+def test_electroncloud_config(test_context):
+    fname_line = XTRACK_TEST_DATA/"lhc_no_bb/line_and_particle.json"
+
+    with open(fname_line, 'r') as fid:
+         input_data = json.load(fid)
+    line = xt.Line.from_dict(input_data['line'])
+    line.particle_ref = xp.Particles(p0c=input_data['particle']['p0c'])
+
+    xfields_test_data_folder = pathlib.Path(
+    __file__).parent.joinpath('../test_data').absolute()
+
+    pinch_filenames = {'mqf' : xfields_test_data_folder/"pyecloud_pinch/refined_Pinch_MTI1.0_MLI1.0_DTO2.0_DLO1.0.h5",
+                       'mqd' : xfields_test_data_folder/"pyecloud_pinch/refined_Pinch_MTI1.0_MLI1.0_DTO2.0_DLO1.0.h5"}
+
+    zeta_max = 0.1
+    ecloud_info = json.load(open(xfields_test_data_folder/"pyecloud_pinch/eclouds.json", "r"))
+
+    reduced_ecloud_info = {
+         'mqf' : { key : ecloud_info['mqf'][key] for key in list(ecloud_info['mqf'].keys())[:5]},
+         'mqd' : { key : ecloud_info['mqd'][key] for key in list(ecloud_info['mqd'].keys())[:5]}
+    }
+
+    twiss_without_ecloud, twiss_with_ecloud = xf.full_electroncloud_setup(line=line, 
+                ecloud_info=reduced_ecloud_info, filenames=pinch_filenames, context=test_context, zeta_max=zeta_max)
+
+
+    assert np.all(np.isclose(twiss_with_ecloud['x'],twiss_without_ecloud['x'], atol=1e-12, rtol=0))
+    assert np.all(np.isclose(twiss_with_ecloud['y'],twiss_without_ecloud['y'], atol=1e-12, rtol=0))
