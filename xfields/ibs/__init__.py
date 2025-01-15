c97746dc703b35408307af81e3f9112b2da497c3
Felix Soubelet <felix.soubelet@protonmail.com>
2025-01-15 15:09:44 +0100

    imports order

--- xfields/ibs/__init__.py
+++ xfields/ibs/__init__.py
@@ -5,8 +5,8 @@
 
 from ._analytical import BjorkenMtingwaIBS, IBSGrowthRates, NagaitsevIBS
 from ._api import configure_intrabeam_scattering, get_intrabeam_scattering_growth_rates
-from ._kicks import IBSAnalyticalKick, IBSKineticKick
 from ._equilibrium import compute_equilibrium_emittances_from_sr_and_ibs
+from ._kicks import IBSAnalyticalKick, IBSKineticKick
 
 __all__ = [
     "BjorkenMtingwaIBS",
