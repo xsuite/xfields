b53718df21a59b2bfef5a606424c0c83165a9f3a
Szymon Łopaciuk <szymon@lopaciuk.eu>
2025-03-28 13:54:58 +0100

    Remove commented code

--- xfields/beam_elements/beambeam3d.py
+++ xfields/beam_elements/beambeam3d.py
@@ -25,7 +25,7 @@ class BeamstrahlungTable(xo.HybridClass):
      particle_id: [1] array index in the xpart.Particles object of the primary macroparticle emitting the photon
      primary_energy: [eV] total energy of primary macroparticle before emission of this photon
      photon_id: [1] counter for photons emitted from the same primary in the same collision with a single slice
-     photon_energy: [eV] total energy of a beamstrahlung photon
+        photon_energy: [eV] total energy of a beamstrahlung photon
      photon_critical_energy (with quantum BS only): [eV] critical energy of a beamstrahlung photon
      rho_inv (with quantum BS only): [m^-1] (Fr/dz) inverse bending radius of the primary macroparticle
      n_avg (with mean BS only): [1] avg. number of emitted beamstrahlung photons from 1 macroparticle in collision with a single slice
@@ -545,23 +545,6 @@ class BeamBeamBiGaussian3D(xt.BeamElement):
                         slices_other_beam_zeta_bin_width_star_beamstrahlung = (
                             self.config_for_update.slicer.bin_widths_beamstrahlung
                                 / np.cos(self.phi))
-
-#        if (slices_other_beam_zeta_bin_width_star_beamstrahlung is None and
-#            slices_other_beam_zeta_bin_width_beamstrahlung is not None):
-#            assert not np.isscalar(slices_other_beam_zeta_bin_width_beamstrahlung), (
-#                'slices_other_beam_zeta_bin_width_beamstrahlung must be an array')
-#            assert (len(slices_other_beam_zeta_bin_width_beamstrahlung)
-#                == len(self.slices_other_beam_num_particles))
-#            slices_other_beam_zeta_bin_width_star_beamstrahlung = (
-#                slices_other_beam_zeta_bin_width_beamstrahlung / np.cos(self.phi))
-#
-#        if slices_other_beam_zeta_bin_width_star_beamstrahlung is not None:
-#            assert not np.isscalar(slices_other_beam_zeta_bin_width_star_beamstrahlung), (
-#                'slices_other_beam_zeta_bin_width_star_beamstrahlung must be an array')
-#            assert (len(slices_other_beam_zeta_bin_width_star_beamstrahlung)
-#                == len(self.slices_other_beam_num_particles))
-#            self.slices_other_beam_zeta_bin_width_star_beamstrahlung = self._arr2ctx(
-#                np.array(slices_other_beam_zeta_bin_width_star_beamstrahlung))
 
         if slices_other_beam_zeta_bin_width_beamstrahlung is not None:
             assert not np.isscalar(slices_other_beam_zeta_bin_width_beamstrahlung), (
