0af063e11fc63149606947bf7c5c54aa8663e724
Frederik F. Van der Veken <frederik@cern.ch>
2025-08-31 19:10:12 +0200

    Specify Xfields elements and inits to be added to the prebuilt kernels.

---
+++ xfields/prebuild_kernels/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xfields package. #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from .kernel_definitions import DEFAULT_XF_ELEMENTS
+from .prebuild_kernels import XF_ELEMENTS_INIT_DEFAULTS
---
+++ xfields/prebuild_kernels/kernel_definitions.py
@@ -0,0 +1,14 @@
+# copyright ############################### #
+# This file is part of the Xfields package. #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from ..beam_elements import *
+
+# Xfields elements
+DEFAULT_XF_ELEMENTS = [
+    BeamBeamBiGaussian2D,
+    BeamBeamBiGaussian3D,
+    SpaceChargeBiGaussian,
+]
+
