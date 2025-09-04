49b473e6e9651108a651f9da51003be14d3dc28e
gbrogginess <giacomo.broggi@cern.ch>
2025-09-04 16:55:15 +0200

    Touschek!

---
+++ xfields/beam_elements/touschek.py
@@ -0,0 +1,201 @@
+import xobjects as xo
+import xtrack as xt
+
+import numpy as np
+from scipy.integrate import quad
+from scipy.special import i0
+from scipy.constants import physical_constants
+
+from ..general import _pkg_root
+
+# ===========================================
+# Constants
+# ===========================================
+ELECTRON_MASS_EV = xt.ELECTRON_MASS_EV
+C_LIGHT_VACUUM = physical_constants['speed of light in vacuum'][0]
+CLASSICAL_ELECTRON_RADIUS = physical_constants['classical electron radius'][0]
+
+class TouschekScattering(xt.BeamElement):
+
+    _xofields = {
+        # All these are handled by the manager for now
+
+        # Everything that need to be accessed with TouschekElementData_get in C
+        '_p0c': xo.Float64,
+        '_bunch_population': xo.Float64,
+        '_gemitt_x': xo.Float64,
+        '_gemitt_y': xo.Float64,
+        '_alfx': xo.Float64,
+        '_betx': xo.Float64,
+        '_alfy': xo.Float64,
+        '_bety': xo.Float64,
+        '_dx': xo.Float64,
+        '_dpx': xo.Float64,
+        '_dy': xo.Float64,
+        '_dpy': xo.Float64,
+        '_deltaN': xo.Float64,
+        '_deltaP': xo.Float64,
+        '_sigma_z': xo.Float64,
+        '_sigma_delta': xo.Float64,
+        '_n_simulated': xo.Int64,
+        '_nx': xo.Float64,
+        '_ny': xo.Float64,
+        '_nz': xo.Float64,
+        '_ignored_portion': xo.Float64,
+        '_integrated_piwinski_rate': xo.Float64,
+    }
+
+    # allow_track = False
+    _depends_on = [xt.RandomUniformAccurate]
+
+    _extra_c_sources = [
+        _pkg_root.joinpath('headers/constants.h'),
+        _pkg_root.joinpath('beam_elements/touschek_src/touschek.h'),
+    ]
+
+    _per_particle_kernels = {
+        '_scatter': xo.Kernel(
+            c_name='TouschekScatter',
+            args=[
+                # xo.Arg(xo.Int64,   name='n_simulated'),
+                # xo.Arg(xo.Float64, name='nx'),
+                # xo.Arg(xo.Float64, name='ny'),
+                # xo.Arg(xo.Float64, name='nz'),
+                # xo.Arg(xo.Float64, name='ignored_portion'),
+
+                # These are variables that we will get out from the C kernel
+                # We will need to pre-allocate memory for them in Python
+                # Then the C kernel will fill them in
+                xo.Arg(xo.Float64, name='x_out', pointer=True),
+                xo.Arg(xo.Float64, name='px_out', pointer=True),
+                xo.Arg(xo.Float64, name='y_out', pointer=True),
+                xo.Arg(xo.Float64, name='py_out', pointer=True),
+                xo.Arg(xo.Float64, name='zeta_out', pointer=True),
+                xo.Arg(xo.Float64, name='delta_out', pointer=True),
+                xo.Arg(xo.Float64, name='weight_out', pointer=True),
+                xo.Arg(xo.Float64, name='totalMCRate_out', pointer=True),
+                xo.Arg(xo.Int64,   name='n_selected_out', pointer=True),
+            ],
+        ),
+    }
+
+    def __init__(self,
+                s=0.0,
+                particle_ref=xt.Particles(),
+                bunch_population=0.0,
+                alfx=0.0, betx=0.0, alfy=0.0, bety=0.0,
+                dx=0.0, dpx=0.0, dy=0.0, dpy=0.0,
+                deltaN=0.0, deltaP=0.0,
+                gemitt_x=0.0, gemitt_y=0.0,
+                sigma_z=0.0, sigma_delta=0.0,
+                n_simulated=0, nx=0.0, ny=0.0, nz=0.0,
+                piwinski_rate=0.0,
+                ignored_portion=0.0,
+                integrated_piwinski_rate=0.0,
+                **kwargs):
+        
+        # This gives AttributeError: 'TouschekScattering' object has no attribute '_xobject'
+        # if not isinstance(self._context, xo.ContextCpu) or self._context.openmp_enabled:
+        #     raise ValueError('TouschekScattering only enabled on CPU.')
+
+        if '_xobject' in kwargs.keys():
+            self.xoinitialize(**kwargs)
+            return
+        
+        super().__init__(**kwargs)
+
+        self._s = s
+        self._particle_ref = particle_ref
+        self._bunch_population = bunch_population
+        self._alfx = alfx
+        self._betx = betx
+        self._alfy = alfy
+        self._bety = bety
+        self._dx = dx
+        self._dpx = dpx
+        self._dy = dy
+        self._dpy = dpy
+        self._deltaN = deltaN
+        self._deltaP = deltaP
+        self._gemitt_x = gemitt_x
+        self._gemitt_y = gemitt_y
+        self._sigma_z = sigma_z
+        self._sigma_delta = sigma_delta
+        self._n_simulated = n_simulated
+        self._nx = nx
+        self._ny = ny
+        self._nz = nz
+        self._ignored_portion = ignored_portion
+        self._integrated_piwinski_rate = integrated_piwinski_rate
+
+        self.piwinski_rate = piwinski_rate
+
+
+    def _configure(self, **kwargs):
+        config_allowed = {
+            "_particle_ref", "_bunch_population",
+            "_gemitt_x", "_gemitt_y",
+            "_alfx", "_betx", "_alfy", "_bety",
+            "_dx", "_dpx", "_dy", "_dpy",
+            "_deltaN", "_deltaP",
+            "_sigma_z", "_sigma_delta",
+            "_n_simulated", "_nx", "_ny", "_nz",
+            "_ignored_portion", "piwinski_rate",
+            "_integrated_piwinski_rate"
+        }
+
+        unknown = set(kwargs) - config_allowed
+        if unknown:
+            bad = ", ".join(sorted(unknown))
+            raise KeyError(f"Unsupported configure() keys: {bad}")
+        
+        for kk, vv in kwargs.items():
+            setattr(self, kk, vv)
+            if kk == "_particle_ref":
+                self._p0c = self._particle_ref.p0c[0]
+
+        
+    def scatter(self):
+        context = self._context
+        particles = xt.Particles(_context=context)
+
+        if not particles._has_valid_rng_state():
+            particles._init_random_number_generator()
+
+        x_out      = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        px_out     = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        y_out      = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        py_out     = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        zeta_out   = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        delta_out  = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        weight_out = context.zeros(shape=(self._n_simulated,), dtype=np.float64)
+        totalMCRate_out = context.zeros(shape=(1,), dtype=np.float64)
+        n_selected_out  = context.zeros(shape=(1,), dtype=np.int64)
+
+        self._scatter(particles=particles,
+                      x_out=x_out, px_out=px_out,
+                      y_out=y_out, py_out=py_out,
+                      zeta_out=zeta_out, delta_out=delta_out,
+                      weight_out=weight_out,
+                      totalMCRate_out=totalMCRate_out,
+                      n_selected_out=n_selected_out)
+        
+        n = n_selected_out[0]
+        part = xt.Particles(_capacity=2*n, 
+                            p0c=self._p0c,
+                            mass0=self._particle_ref.mass0,
+                            q0=self._particle_ref.q0, 
+                            pdg_id=self._particle_ref.pdg_id,
+                            x=x_out[:n], px=px_out[:n],
+                            y=y_out[:n], py=py_out[:n],
+                            zeta=zeta_out[:n], delta=delta_out[:n],
+                            weight=weight_out[:n],
+                            s=getattr(self, '_s', 0.0))
+        
+        self.particles = part
+        self.total_mc_rate = totalMCRate_out
+        self.ignored_rate = self._ignored_portion * self.total_mc_rate
+
+    
+    def track(self, particles):
+        super().track(particles)
---
+++ xfields/touschek/manager.py
@@ -0,0 +1,305 @@
+import xtrack as xt
+import xfields as xf
+
+import numpy as np
+from scipy.integrate import quad
+from scipy.special import i0
+from scipy.constants import physical_constants
+
+# ===========================================
+# Constants
+# ===========================================
+ELECTRON_MASS_EV = xt.ELECTRON_MASS_EV
+C_LIGHT_VACUUM = physical_constants['speed of light in vacuum'][0]
+CLASSICAL_ELECTRON_RADIUS = physical_constants['classical electron radius'][0]
+
+class TouschekCalculator:
+    def __init__(self, manager):
+        self.manager = manager
+        self.twiss = None
+
+    def _compute_piwinski_integral(self, tm, B1, B2):
+        km = np.arctan(np.sqrt(tm))
+
+        def int_piwinski(k, km, B1, B2):
+            t = np.tan(k) ** 2
+            tm = np.tan(km) ** 2
+            fact = (
+                (2*t + 1)**2 * (t/tm / (1+t) - 1) / t + t - np.sqrt(t*tm * (1 + t))
+                - (2 + 1 / (2*t)) * np.log(t/tm / (1+t))
+            )
+            if B2 * t < 500:
+                intp = fact * np.exp(-B1*t) * i0(B2*t) * np.sqrt(1+t)
+            else:
+                intp = (
+                    fact
+                    * np.exp(B2*t - B1*t)
+                    / np.sqrt(2*np.pi * B2*t)
+                    * np.sqrt(1+t)
+                )
+            return intp
+
+        args = (km, B1, B2)
+        val, _ =  quad(
+            int_piwinski,
+            km,
+            np.pi / 2,
+            args=args,
+            epsabs=1e-16,
+            epsrel=1e-12
+        )
+
+        return val
+
+    def _compute_piwinski_scattering_rate(self, element):
+        p0c = self.manager.particle_ref.p0c[0]
+        bunch_population = self.manager.bunch_population
+        gemitt_x = self.manager.gemitt_x
+        gemitt_y = self.manager.gemitt_y
+        alfx = self.twiss['alfx', element]
+        betx = self.twiss['betx', element]
+        alfy = self.twiss['alfy', element]
+        bety = self.twiss['bety', element]
+        sigma_z = self.manager.sigma_z
+        sigma_delta = self.manager.sigma_delta
+        delta = self.twiss['delta', element]
+        dx = self.twiss['dx', element]
+        dpx = self.twiss['dpx', element]
+        dxt = alfx * dx + betx * dpx # dxt: dx tilde
+        dy = self.twiss['dy', element]
+        dpy = self.twiss['dpy', element]
+        dyt = alfy * dy + bety * dpy # dyt: dy tilde
+
+        deltaN = self.manager.momentum_aperture.at[element, "deltaN"]
+        deltaP = self.manager.momentum_aperture.at[element, "deltaP"]
+
+        sigmab_x = np.sqrt(gemitt_x * betx) # Horizontal betatron beam size
+        sigma_x = np.sqrt(gemitt_x * betx + dx**2 * sigma_delta**2) # Horizontal beam size
+
+        sigmab_y = np.sqrt(gemitt_y * bety) # Vertical betatron beam size
+        sigma_y = np.sqrt(gemitt_y * bety + dy**2 * sigma_delta**2) # Vertical beam size
+
+        sigma_h = (sigma_delta**-2 + (dx**2 + dxt**2)/sigmab_x**2 + (dy**2 + dyt**2)/sigmab_y**2)**(-0.5)
+
+        p = p0c * (1 + delta)
+        gamma = np.sqrt(1 + p**2 / ELECTRON_MASS_EV**2)
+        beta = np.sqrt(1 - gamma**-2)
+
+        B1 = betx**2 / (2 * beta**2 * gamma**2 * sigmab_x**2) * (1 - sigma_h**2 * dxt**2 / sigmab_x**2) \
+             + bety**2 / (2 * beta**2 * gamma**2 * sigmab_y**2) * (1 - sigma_h**2 * dyt**2 / sigmab_y**2)
+
+        B2 = np.sqrt(B1**2 - betx**2 * bety**2 * sigma_h**2 / (beta**4 * gamma**4 * sigmab_x**4 * sigmab_y**4 * sigma_delta**2) \
+                             * (sigma_x**2 * sigma_y**2 - sigma_delta**4 * dx**2 * dy**2))
+
+        tmN = beta**2 * (deltaN**2)
+        tmP = beta**2 * (deltaP**2)
+
+        piwinski_integralN = self._compute_piwinski_integral(tmN, B1, B2)
+        piwinski_integralP = self._compute_piwinski_integral(tmP, B1, B2)
+
+        rateN = CLASSICAL_ELECTRON_RADIUS**2 * C_LIGHT_VACUUM * bunch_population**2 \
+                / (8*np.pi * gamma**2 * sigma_z * np.sqrt(sigma_x**2 * sigma_y**2 - sigma_delta**4 * dx**2 * dy**2)) \
+                * 2 * np.sqrt(np.pi * (B1**2 - B2**2)) * piwinski_integralN
+
+        rateP = CLASSICAL_ELECTRON_RADIUS**2 * C_LIGHT_VACUUM * bunch_population**2 \
+                / (8*np.pi * gamma**2 * sigma_z * np.sqrt(sigma_x**2 * sigma_y**2 - sigma_delta**4 * dx**2 * dy**2)) \
+                * 2 * np.sqrt(np.pi * (B1**2 - B2**2)) * piwinski_integralP
+
+        rate = (rateN + rateP) / 2
+
+        return rate
+
+    def _compute_integrated_piwinski_rates(self):
+        """
+        Integrate Piwinski Touschek scattering rate along s using trapezoidal rule and
+        store the per-TouschekElelement the integrated rate per bunch.
+        """
+        line = self.manager.line
+        tab = line.get_table()
+        T_rev0 = float(self.twiss.T_rev0)
+
+        # Indexes of the TouschekScatterings
+        ii_t = [ii for ii, nn in enumerate(tab.name[:-1]) if isinstance(line[nn], xf.TouschekScattering)]
+
+        integrated = 0.0
+        s0 = 0.0
+        r0 = self._compute_piwinski_scattering_rate(tab.name[0])
+
+        s_before = s0
+        rate_before = r0
+        ii_current = 0
+
+        for ii, nn in enumerate(tab.name):
+            s = tab.rows[nn].s[0]
+            ds = s - s_before
+            if ds > 0.0:
+                rate = self._compute_piwinski_scattering_rate(nn)
+                integrated += 0.5 * (rate_before + rate) * ds
+                s_before = s
+                rate_before = rate
+
+            if ii_current < len(ii_t) and ii == ii_t[ii_current]:
+                # divide by c and by T_rev0 -> per-bunch rate
+                integrated_piwinski_rate = integrated / C_LIGHT_VACUUM / T_rev0
+                elem = line[nn] # xf.TouschekScattering
+                elem._configure(_integrated_piwinski_rate=integrated_piwinski_rate)
+                integrated = 0.0
+                ii_current += 1
+                if ii_current >= len(ii_t):
+                    break
+
+class TouschekManager:
+    def __init__(self, line, momentum_aperture, nemitt_x=None, nemitt_y=None,
+                 sigma_z=None, sigma_delta=None, bunch_population=None,
+                 n_simulated=None, gemitt_x=None, gemitt_y=None,
+                 momentum_aperture_scale=0.85, ignored_portion=0.01,
+                 nx=3, ny=3, nz=3, **kwargs):
+
+        # Input validation
+        if line is None:
+            raise ValueError("`line` is required.")
+        if not hasattr(line, "particle_ref"):
+            raise ValueError("`line` must have a `particle_ref`.")
+        if momentum_aperture is None:
+            raise ValueError("`momentum_aperture` is required.")
+        if sigma_z is None:
+            raise ValueError("`sigma_z` is required.")
+        if sigma_delta is None:
+            raise ValueError("`sigma_delta` is required.")
+        if bunch_population is None:
+            raise ValueError("`bunch_population` is required.")
+        if n_simulated is None:
+            raise ValueError("`n_simulated` is required.")
+
+        # Momentum aperture validation
+        required_cols = {"s", "name", "deltaN", "deltaP"}
+        if not hasattr(momentum_aperture, "columns") or not hasattr(momentum_aperture, "__getitem__"):
+            raise TypeError("`momentum_aperture` must be a DataFrame-like object with columns "
+                            "'s', 'name', 'deltaN', 'deltaP'.")
+        missing = required_cols - set(momentum_aperture.columns)
+        if missing:
+            raise ValueError(f"`momentum_aperture` missing columns: {sorted(missing)}")
+
+        for col in ("s", "deltaN", "deltaP"):
+            try:
+                vals = momentum_aperture[col].astype(float)
+            except Exception:
+                raise TypeError(f"`{col}` column must be numeric (cannot coerce to float).")
+            if not vals.notna().all():
+                bad = list(vals.index[~vals.notna()][:5])
+                raise ValueError(f"`{col}` contains NaN at rows {bad}.")
+            if (abs(vals) == float("inf")).any():
+                bad = list(vals.index[(abs(vals) == float("inf"))][:5])
+                raise ValueError(f"`{col}` contains inf at rows {bad}.")
+
+        self.line = line
+        self.particle_ref = line.particle_ref
+
+        momentum_aperture = momentum_aperture.copy()
+        momentum_aperture['deltaN'] *= momentum_aperture_scale
+        momentum_aperture['deltaP'] *= momentum_aperture_scale
+        self.momentum_aperture= momentum_aperture.set_index("name")
+
+        self.sigma_z = sigma_z
+        self.sigma_delta = sigma_delta
+        self.bunch_population = bunch_population
+        self.n_simulated = n_simulated
+        self.momentum_aperture_scale = momentum_aperture_scale
+        self.ignored_portion = ignored_portion
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+
+        # Emittance validation
+        nemitt_given = nemitt_x is not None and nemitt_y is not None
+        gemitt_given = gemitt_x is not None and gemitt_y is not None
+
+        if nemitt_given and gemitt_given:
+            raise ValueError("Provide either normalized emittances (nemitt_x, nemitt_y) "
+                             "OR geometric emittances (gemitt_x, gemitt_y), not both.")
+        if not (nemitt_given or gemitt_given):
+            raise ValueError("You must provide either both normalized emittances (nemitt_x, nemitt_y) "
+                             "OR both geometric emittances (gemitt_x, gemitt_y).")
+
+        if nemitt_given:
+            beta0 = line.particle_ref.beta0[0]
+            gamma0 = line.particle_ref.gamma0[0]
+            self.gemitt_x = nemitt_x / (beta0 * gamma0)
+            self.gemitt_y = nemitt_y / (beta0 * gamma0)
+        else:
+            self.gemitt_x = gemitt_x
+            self.gemitt_y = gemitt_y
+
+        self.kwargs = kwargs
+
+        self.touschek = TouschekCalculator(self)
+
+        # Check that the line contains TouschekScatterings
+        tab = self.line.get_table()
+        try:
+            has = "TouschekScattering" in set(np.unique(tab.element_type))
+        except Exception:
+            has = "TouschekScattering" in set(getattr(tab, "element_type", []))
+        if not has:
+            raise ValueError("The line does not contain any TouschekScattering. "
+                             "Please add them before initializing the TouschekManager.")
+
+
+    def initialise_touschek(self, element=None):
+        line = self.line
+        tab = line.get_table()
+
+        twiss_method = self.kwargs.get("method", "6d")
+        twiss = self.line.twiss(method=twiss_method)
+        # Pass the twiss to the TouschekCalculator
+        self.touschek.twiss = twiss
+
+        self.touschek._compute_integrated_piwinski_rates()
+
+        # Helper to assign all fields to a single TouschekScattering
+        def _assign(nn: str):
+            alfx = twiss["alfx", nn]; betx = twiss["betx", nn]
+            alfy = twiss["alfy", nn]; bety = twiss["bety", nn]
+            dx   = twiss["dx",   nn]; dpx = twiss["dpx",  nn]
+            dy   = twiss["dy",   nn]; dpy = twiss["dpy",  nn]
+            dN = self.momentum_aperture.at[nn, "deltaN"]
+            dP = self.momentum_aperture.at[nn, "deltaP"]
+
+            piwinski_rate = self.touschek._compute_piwinski_scattering_rate(nn)
+
+            elem = line[nn] # xf.TouschekScattering
+
+            elem._configure(
+                _particle_ref=self.particle_ref,
+                _bunch_population=self.bunch_population,
+                _gemitt_x=self.gemitt_x,
+                _gemitt_y=self.gemitt_y,
+                _alfx=alfx, _betx=betx,
+                _alfy=alfy, _bety=bety,
+                _dx=dx, _dpx=dpx,
+                _dy=dy, _dpy=dpy,
+                _deltaN=dN, _deltaP=dP,
+                _sigma_z=self.sigma_z,
+                _sigma_delta=self.sigma_delta,
+                _n_simulated=self.n_simulated,
+                _nx=self.nx, _ny=self.ny, _nz=self.nz,
+                _ignored_portion=self.ignored_portion,
+                piwinski_rate=piwinski_rate,
+            )
+
+        if element is None:
+            for nn in tab.name[:-1]: # Avoid the last tab.name which is _end_point
+                if isinstance(line[nn], xf.TouschekScattering):
+                    _assign(nn)
+        else:
+            if not isinstance(element, str):
+                raise TypeError(f"`element` must be a string (got {type(element).__name__}).")
+            if element not in set(tab.name):
+                raise ValueError(
+                    f"`element='{element}'` is not present in the line provided to the TouschekManager."
+                )
+            if not isinstance(line[element], xf.TouschekScattering):
+                raise TypeError(
+                    f"`line['{element}']` is not a TouschekScattering (got {type(line[element]).__name__})."
+                )
+            _assign(element)
