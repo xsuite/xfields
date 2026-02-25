93d34a08e7c404a7df5a4842e046177f9afe9468
giadarol <giovanni.iadarola@cern.ch>
2026-02-25 15:07:48 +0100

    declare NON_TRACKING_ELEMENTS

--- xfields/prebuilt_kernel_definitions/element_types.py
+++ xfields/prebuilt_kernel_definitions/element_types.py
@@ -3,14 +3,14 @@
 # Copyright (c) CERN, 2025.                 #
 # ######################################### #
 
-from ..beam_elements.beambeam2d import BeamBeamBiGaussian2D
-from ..beam_elements.beambeam3d import BeamBeamBiGaussian3D
-from ..beam_elements.spacecharge import SpaceChargeBiGaussian
-
+import xfields as xf
 
 DEFAULT_XFIELDS_ELEMENTS = [
-    BeamBeamBiGaussian2D,
-    BeamBeamBiGaussian3D,
-    SpaceChargeBiGaussian,
+    xf.BeamBeamBiGaussian2D,
+    xf.BeamBeamBiGaussian3D,
+    xf.SpaceChargeBiGaussian,
 ]
 
+NON_TRACKING_ELEMENTS = [
+    xf.beam_elements.temp_slicer.TempSlicer
+]
